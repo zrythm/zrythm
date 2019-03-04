c8b5ee1520b8f9002b785bbb8ba40326dc9640a1
Alexandros Theodotou <alex@zrythm.org>
2019-03-04 22:23:55 +0000

    remove packaging related stuff from master

--- scripts/package-deb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir -p $_PATH
-cp $PACKAGE_NAME.tar.gz $_PATH/$PACKAGE_NAME.orig.tar.gz
-tar xf $PACKAGE_NAME.tar.gz --directory $_PATH/
-cd $_PATH/$PACKAGE_NAME
-debuild -us -uc
--- scripts/package-rpm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd
-tar xvzf $PACKAGE_NAME.tar.gz
-cp $PACKAGE_NAME/zrythm.spec rpmbuild/SPECS/
-cp $PACKAGE_NAME.tar.gz rpmbuild/SOURCES/
-cd rpmbuild/SPECS/
-rpmbuild -ba zrythm.spec
