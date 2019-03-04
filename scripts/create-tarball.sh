4e5308ced2b639baf2c13e8539739676685835a7
Alexandros Theodotou <alex@zrythm.org>
2019-03-04 22:23:55 +0000

    remove packaging related stuff from master

--- scripts/create-tarball.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir $PACKAGE_NAME
-mv autogen.sh debian ext config.guess config.sub \
-  configure.ac CONTRIBUTING.md data Doxyfile inc INSTALL.md \
-  install-sh LICENSE Makefile.in README.md resources \
-  zrythm.spec src $PACKAGE_NAME/
-tar -zcvf $PACKAGE_NAME.tar.gz $PACKAGE_NAME
