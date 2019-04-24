bd0232c96cf70721a579341481c847d1e819c3ad
Alexandros Theodotou <alex@zrythm.org>
2019-04-25 00:30:45 +0100

    fix trying to open folder that might not exist

---
+++ inc/plugins/lv2/log.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2019 Alexandros Theodotou <alex at zrythm dot org>
+ *
+ * This file is part of Zrythm
+ *
+ * Zrythm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Zrythm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Zrythm.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file
+ *
+ * LV2 Log feature implementations.
+ */
+
+#include <lv2/lv2plug.in/ns/ext/log/log.h>
+
+/**
+ * @addtogroup lv2
+ *
+ * @{
+ */
+
+int
+lv2_vprintf(LV2_Log_Handle handle,
+             LV2_URID       type,
+             const char*    fmt,
+             va_list        ap);
+
+int
+lv2_printf(LV2_Log_Handle handle,
+            LV2_URID       type,
+            const char*    fmt, ...);
+
+/**
+ * @}
+ */
