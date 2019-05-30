09edc791e911a0de301c146fd6edfe520c1338b3
Alexandros Theodotou <alex@zrythm.org>
2019-05-30 04:28:58 +0100

    added missing meson install for gnu/linux

--- scripts/collect_translatables.sh
+++ scripts/collect_translatables.sh
@@ -4,6 +4,7 @@ import os
 import os.path
 import sys
 
-for dirpath, dirnames, filenames in os.walk(sys.argv[1]):
-  for filename in [f for f in filenames if f.endswith(".c") or f.endswith(".h") or f.endswith(".ui")]:
-    print(os.path.join(dirpath, filename))
+for path in ['inc', 'src', 'resources']:
+  for dirpath, dirnames, filenames in os.walk(path):
+    for filename in [f for f in filenames if f.endswith(".c") or f.endswith(".h") or f.endswith(".ui")]:
+      print(os.path.join(dirpath, filename))
