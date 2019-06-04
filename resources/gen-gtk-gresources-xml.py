ccee5e4701c8e0dac45dc271668c8dbd1bfa10fc
Alexandros Theodotou <alex@zrythm.org>
2019-06-04 12:06:21 +0100

    fix icons not showing in meson build

--- resources/gen-gtk-gresources-xml.py
+++ resources/gen-gtk-gresources-xml.py
@@ -91,104 +91,97 @@ def remove_prefix(text, prefix):
   return text
 
 icons_dir = 'icons/breeze-icons'
+_breeze_icons = [
+  'window-minimize',
+  'media-record',
+  'media-playback-start',
+  'media-playback-stop',
+  'media-optical-audio',
+  'document-properties',
+  'media-seek-backward',
+  'media-seek-forward',
+  'media-playlist-repeat',
+  'draw-line',
+  'audio-speakers-symbolic',
+  'application-msword',
+  'zoom-in',
+  'zoom-out',
+  'zoom-fit-best',
+  'zoom-original',
+  'node-type-cusp',
+  'text-x-csrc',
+  'audio-headphones',
+  'visibility',
+  'view-fullscreen',
+  'format-justify-fill',
+  'audio-midi',
+  'audio-mp3',
+  'audio-flac',
+  'application-ogg',
+  'dialog-messages',
+  'audio-x-wav',
+  'inode-directory',
+  'none',
+  'audio-card',
+  'minuet-chords',
+  'emblem-symbolic-link',
+  'window-close',
+  'window-close-symbolic',
+  'edit-select-symbolic',
+  'editor',
+  'document-new',
+  'document-open',
+  'document-save',
+  'document-save-as',
+  'document-send',
+  'application-x-m4',
+  'gtk-quit',
+  'edit-undo',
+  'edit-redo',
+  'edit-cut',
+  'edit-copy',
+  'edit-paste',
+  'edit-delete',
+  'edit-select-all',
+  'kt-show-statusbar',
+  'edit-clear',
+  'select-rectangular',
+  'media-repeat-album-amarok',
+  'help-about',
+  'help-donate',
+  'help-contents',
+  'configure-shortcuts',
+  'tools-report-bug',
+  'delete',
+  'edit-duplicate',
+  'applications-internet',
+  'draw-eraser',
+  'audio-speakers-symbolic',
+  'step_object_Controller',
+  'gtk-add',
+  'kdenlive-show-markers',
+  'document-duplicate',
+  'mathmode',
+  'distortionfx',
+  'selection-end-symbolic',
+  'edit-select',
+  'plugins',
+  'hand',
+  'window-maximize',
+  ]
 for cat in ['actions', 'animations', 'applets', 'apps', 'categories', 'devices', 'emblems', 'emotes', 'mimetypes', 'places', 'preferences', 'status']:
-  for size in ['12', '16', '22', '24', '32', '64']:
-    destdir = '{0}x{0}/{1}'.format(size,cat)
-    full_destdir = os.path.join(icons_dir,destdir)
-    if os.path.exists(os.path.join(srcdir,full_destdir)):
-      for f in get_files(full_destdir, '.svg'):
-        destfile = os.path.join(full_destdir,f)
-        if (f.endswith('window-minimize.svg') or
-              f.endswith('emblem-symbolic-link.svg') or
-              f.endswith('audio-card.svg') or
-              f.endswith('window-close.svg') or
-              f.endswith('media-record.svg') or
-              f.endswith('media-playback-start.svg') or
-              f.endswith('media-playback-stop.svg') or
-              f.endswith('media-optical-audio.svg') or
-              f.endswith('document-properties') or
-              f.endswith('media-seek-backward.svg') or
-              f.endswith('media-seek-forward.svg') or
-              f.endswith('media-playlist-repeat.svg') or
-              f.endswith('window-close-symbolic.svg') or
-              f.endswith('edit-select.svg') or
-              f.endswith('editor.svg') or
-              f.endswith('draw-eraser.svg') or
-              f.endswith('draw-line.svg') or
-              f.endswith('audio-speakers-symbolic.svg') or
-              f.endswith('application-msword.svg') or
-              f.endswith('gtk-add.svg') or
-              f.endswith('zoom-in.svg') or
-              f.endswith('zoom-out.svg') or
-              f.endswith('zoom-fit-best.svg') or
-              f.endswith('zoom-original.svg') or
-              f.endswith('node-type-cusp.svg') or
-              f.endswith('text-x-csrc.svg') or
-              f.endswith('audio-headphones.svg') or
-              f.endswith('document-properties.svg') or
-              f.endswith('visibility.svg') or
-              f.endswith('view-fullscreen.svg') or
-              f.endswith('format-justify-fill.svg') or
-              f.endswith('media-optical-audio.svg') or
-              f.endswith('audio-midi.svg') or
-              f.endswith('audio-mp3.svg') or
-              f.endswith('audio-flac.svg') or
-              f.endswith('application-ogg.svg') or
-              f.endswith('dialog-messages.svg') or
-              f.endswith('audio-x-wav.svg') or
-              f.endswith('inode-directory.svg') or
-              f.endswith('none.svg') or
-              f.endswith('audio-card.svg') or
-              f.endswith('minuet-chords.svg') or
-              f.endswith('emblem-symbolic-link.svg') or
-              f.endswith('window-minimize.svg') or
-              f.endswith('window-close.svg') or
-              f.endswith('window-close-symbolic.svg') or
-              f.endswith('edit-select-symbolic.svg') or
-              f.endswith('editor.svg') or
-              f.endswith('document-new.svg') or
-              f.endswith('document-open.svg') or
-              f.endswith('document-save.svg') or
-              f.endswith('document-save-as.svg') or
-              f.endswith('document-send.svg') or
-              f.endswith('application-x-m4.svg') or
-              f.endswith('gtk-quit.svg') or
-              f.endswith('edit-undo.svg') or
-              f.endswith('edit-redo.svg') or
-              f.endswith('edit-cut.svg') or
-              f.endswith('edit-copy.svg') or
-              f.endswith('edit-paste.svg') or
-              f.endswith('edit-delete.svg') or
-              f.endswith('edit-select-all.svg') or
-              f.endswith('kt-show-statusbar.svg') or
-              f.endswith('edit-clear.svg') or
-              f.endswith('select-rectangular.svg') or
-              f.endswith('media-repeat-album-amarok.svg') or
-              f.endswith('help-about.svg') or
-              f.endswith('help-donate.svg') or
-              f.endswith('help-contents.svg') or
-              f.endswith('configure-shortcuts.svg') or
-              f.endswith('tools-report-bug.svg') or
-              f.endswith('delete.svg') or
-              f.endswith('edit-duplicate.svg') or
-              f.endswith('applications-internet.svg') or
-              f.endswith('draw-eraser.svg') or
-              f.endswith('draw-line.svg') or
-              f.endswith('audio-speakers-symbolic.svg') or
-              f.endswith('step_object_Controller.svg') or
-              f.endswith('application-msword.svg') or
-              f.endswith('gtk-add.svg') or
-              f.endswith('kdenlive-show-markers.svg') or
-              f.endswith('document-duplicate.svg') or
-              f.endswith('mathmode.svg') or
-              f.endswith('distortionfx.svg') or
-              f.endswith('selection-end-symbolic.svg') or
-              f.endswith('edit-select.svg') or
-              f.endswith('plugins.svg') or
-              f.endswith('hand.svg') or
-              f.endswith('window-maximize.svg')):
-          xml += '    <file>{0}</file>\n'.format(remove_prefix(destfile, srcdir + os.sep))
-    full_destdir = os.path.join(icons_dir,destdir)
+  for size in ['12', '16', '22', '24', '32', '64', 'sybmolic']:
+    src_dir = '{0}/{1}'.format(cat,size)
+    alias_dir = '{0}x{0}/{1}'.format(size,cat)
+    full_src_dir = os.path.join(icons_dir,src_dir)
+    full_alias_dir = os.path.join(icons_dir,alias_dir)
+    if os.path.exists(os.path.join(srcdir,full_src_dir)):
+      for icon_name in _breeze_icons:
+        for f in get_files(full_src_dir, '.svg'):
+          if (f == (icon_name + '.svg')):
+            icon_path = os.path.join(full_src_dir,f)
+            alias = os.path.join(full_alias_dir,'z-' + f)
+            xml += '    <file alias=\"{0}\">{1}</file>\n'.format(alias,remove_prefix(icon_path, srcdir + os.sep))
 
 xml += '''
   <file>theme.css</file>
