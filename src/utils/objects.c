919356179f3dfeafe6537ab4b31999b0129af488
Alexandros Theodotou <alex@zrythm.org>
2020-05-12 18:44:18 +0100

    traverse the object utils free-stack in a non-gtk thread (fixes freezes when it has lots of objects)

--- src/utils/objects.c
+++ src/utils/objects.c
@@ -22,6 +22,8 @@
 #include "zrythm.h"
 
 static Stack * free_stack = NULL;
+static Stack * free_stack_for_source = NULL;
+static ZixSem free_stack_for_source_lock;
 
 /** Time to wait in seconds for object deletion. */
 #define TIME_TO_WAIT_SEC 20
@@ -41,56 +43,94 @@ typedef struct FreeElement
   void (*dfunc) (void *);
 } FreeElement;
 
-/*
- * FIXME why is this source? move to separate
- * thread */
 static gboolean
 free_later_source ()
 {
-  if (!free_stack)
+  zix_sem_wait (&free_stack_for_source_lock);
+
+  /* it might change so get its size at this
+   * point. */
+  int ssize =
+    MIN (stack_size (free_stack_for_source), 28);
+
+  FreeElement * el;
+  for (int i = 0; i < ssize; i++)
     {
-      g_critical ("free_stack uninitialized");
-      return FALSE;
+      /* peek the element at 0 */
+      el =
+        (FreeElement *)
+        stack_pop (free_stack_for_source);
+      el->dfunc (el->obj);
+      free (el);
     }
 
-  if (!stack_is_empty (free_stack))
+  zix_sem_post (&free_stack_for_source_lock);
+
+  return G_SOURCE_CONTINUE;
+}
+
+/*
+ * this thread is only for traversing
+ * the stack and checking the time. if an object
+ * needs to be deleted, it is deferred to
+ * the source above. */
+static gpointer
+free_later_thread_func (
+  gpointer data)
+{
+  while (true)
     {
-      /* it might change so get its size at this
-       * point. */
-      int ssize = stack_size (free_stack);
-      gint64 curr_time =
-        g_get_monotonic_time ();
+      g_usleep (800000);
 
-      FreeElement * el;
-      for (int i = 0; i < ssize; i++)
+      if (!free_stack)
         {
-          /* peek the element at 0 */
-          el =
-            (FreeElement *)
-            stack_peek_last (free_stack);
+          g_critical ("free_stack uninitialized");
+          return NULL;
+        }
 
-          g_warn_if_fail (el != NULL);
+      if (!stack_is_empty (free_stack))
+        {
+          /* it might change so get its size at this
+           * point. */
+          int ssize = stack_size (free_stack);
+          gint64 curr_time =
+            g_get_monotonic_time ();
 
-          /* if enough time has passed */
-          if (curr_time - el->time_added >
-              TIME_TO_WAIT_USEC)
+          FreeElement * el;
+          for (int i = 0; i < ssize; i++)
             {
-              /* pop and free */
+              /* peek the element at 0 */
               el =
                 (FreeElement *)
-                stack_pop_last (free_stack);
-              el->dfunc (el->obj);
-              free (el);
-            }
-          else
-            {
-              /* not enough time passed, exit */
-              break;
+                stack_peek_last (free_stack);
+
+              g_warn_if_fail (el != NULL);
+
+              /* if enough time has passed */
+              if (curr_time - el->time_added >
+                  TIME_TO_WAIT_USEC)
+                {
+                  /* pop and free */
+                  el =
+                    (FreeElement *)
+                    stack_pop_last (free_stack);
+                  zix_sem_wait (
+                    &free_stack_for_source_lock);
+                  stack_push (
+                    free_stack_for_source,
+                    (void *) el);
+                  zix_sem_post (
+                    &free_stack_for_source_lock);
+                }
+              else
+                {
+                  /* not enough time passed, exit */
+                  break;
+                }
             }
         }
     }
-
-  return G_SOURCE_CONTINUE;
+  g_warn_if_reached ();
 }
 
 /**
@@ -128,8 +168,12 @@ _free_later (
 void
 object_utils_init ()
 {
-  free_stack = stack_new (80000);
+  free_stack = stack_new (800000);
+  free_stack_for_source = stack_new (8000);
+  zix_sem_init (&free_stack_for_source_lock, 1);
 
-  /* FIXME don't do it on gtk thread */
-  g_timeout_add (2000, free_later_source, NULL);
+  g_timeout_add (1000, free_later_source, NULL);
+  g_thread_new (
+    "obj_utils_free_later", free_later_thread_func,
+    NULL);
 }
