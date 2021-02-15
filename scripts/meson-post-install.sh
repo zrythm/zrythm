be3af6be6a2ae31089b9eb3b58c1903dcaa801c2
Alexandros Theodotou <alex@zrythm.org>
2021-02-15 18:46:52 +0000

    convert meson postinstall script to shell, use custom repos for lv2-related subprojects

---
+++ scripts/meson-post-install.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env sh
+#
+# Copyright (C) 2019-2021 Alexandros Theodotou <alex at zrythm dot org>
+#
+# This file is part of Zrythm
+#
+# Zrythm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Zrythm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Zrythm.  If not, see <https://www.gnu.org/licenses/>.
+
+suffix="$MESON_INSTALL_PREFIX"
+if [ ! "$suffix" ]; then
+  suffix="/usr/local"
+fi
+prefix="$suffix"
+if [ "$MESON_INSTALL_DESTDIR_PREFIX" ]; then
+  prefix="$MESON_INSTALL_DESTDIR_PREFIX/$prefix"
+fi
+datadir="$prefix/share"
+schemadir="$datadir/glib-2.0/schemas"
+fontsdir="$datadir/fonts/zrythm"
+desktop_db_dir="$datadir/applications"
+mime_dir="$datadir/mime"
+doc_dir="$datadir/doc/zrythm"
+
+if [ ! "$MESON_INSTALL_DESTDIR_PREFIX" ]; then
+  echo "Compiling gsettings schemas..."
+  glib-compile-schemas $schemadir
+
+  if command -v gtk-update-icon-cache; then
+    echo "Updating icon cache..."
+    touch "$datadir/icons/hicolor"
+    gtk-update-icon-cache
+  fi
+
+  if command -v update-mime-database; then
+    echo "Updating MIME database..."
+    update-mime-database "$mime_dir"
+  fi
+
+  if command -v update-desktop-database; then
+    echo "Updating desktop database..."
+    if [ ! -e  "$desktop_db_dir" ]; then
+      mkdir -p "$desktop_db_dir"
+    fi
+    update-desktop-database -q "$desktop_db_dir"
+  fi
+
+  if command -v update-gdk-pixbuf-loaders; then
+    update-gdk-pixbuf-loaders
+  fi
+fi
--- scripts/meson_dist.sh
+++ scripts/meson_dist.sh
@@ -2,6 +2,7 @@
 
 # remove CI scripts
 rm -rf "${MESON_DIST_ROOT}"/.builds
+rm -rf "${MESON_DIST_ROOT}"/.builds-extra
 rm -rf "${MESON_DIST_ROOT}"/.travis.yml
 rm -rf "${MESON_DIST_ROOT}"/.github
 rm -rf "${MESON_DIST_ROOT}"/git-packaging-hooks
