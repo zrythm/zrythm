ffc04226926f47b119be9dc16ce39dd0de24cfa5
Alexandros Theodotou <alex@zrythm.org>
2021-03-09 16:48:12 +0000

    add emblemed icons in plugin browser
    Add emblems to tab icons in the plugin browser if anything is selected.
    
    Implements https://todo.sr.ht/~alextee/zrythm-feature/274.

--- src/gui/widgets/plugin_browser.c
+++ src/gui/widgets/plugin_browser.c
@@ -66,9 +66,149 @@ enum
   PL_NUM_COLUMNS
 };
 
+#define PROTOCOLS_ICON "protocol"
+#define COLLECTIONS_ICON "favorite"
+#define CATEGORIES_ICON "category"
+
 void
 plugin_browser_widget_refresh_collections (
   PluginBrowserWidget * self);
+
+static void
+update_stack_switcher_emblems (
+  PluginBrowserWidget * self)
+{
+  bool has_collections =
+    gtk_tree_selection_count_selected_rows (
+      gtk_tree_view_get_selection (
+        GTK_TREE_VIEW (
+          self->collection_tree_view)));
+  bool has_categories =
+    gtk_tree_selection_count_selected_rows (
+      gtk_tree_view_get_selection (
+        GTK_TREE_VIEW (
+          self->category_tree_view)));
+  bool has_protocols =
+    gtk_tree_selection_count_selected_rows (
+      gtk_tree_view_get_selection (
+        GTK_TREE_VIEW (
+          self->protocol_tree_view)));
+
+  GList *children, *iter;
+  children =
+    gtk_container_get_children (
+      GTK_CONTAINER (self->stack_switcher));
+  for (iter = children;
+       iter != NULL;
+       iter = g_list_next (iter))
+    {
+      if (!GTK_IS_RADIO_BUTTON (iter->data))
+        continue;
+
+      GtkButton * button = GTK_BUTTON (iter->data);
+      GtkWidget * btn_child =
+        gtk_bin_get_child (GTK_BIN (button));
+      GtkImage * prev_img = NULL;
+      if (GTK_IS_BIN (btn_child))
+        {
+          GtkWidget * inner_child =
+            gtk_bin_get_child (
+              GTK_BIN (btn_child));
+          if (GTK_IS_CONTAINER (inner_child))
+            {
+              prev_img =
+                GTK_IMAGE (
+                  z_gtk_container_get_single_child (
+                    GTK_CONTAINER (inner_child)));
+            }
+          else if (GTK_IS_IMAGE (inner_child))
+            {
+              prev_img = GTK_IMAGE (inner_child);
+            }
+          else
+            {
+              g_critical (
+                "unknown type %s",
+                G_OBJECT_TYPE_NAME (inner_child));
+            }
+        }
+      else
+        {
+          prev_img =
+            GTK_IMAGE (
+              gtk_bin_get_child (GTK_BIN (button)));
+        }
+
+      GtkIconSize icon_size;
+      const char * icon_name = NULL;
+      GtkImageType image_type =
+        gtk_image_get_storage_type (prev_img);
+      if (image_type == GTK_IMAGE_ICON_NAME)
+        {
+          gtk_image_get_icon_name (
+            prev_img, &icon_name, &icon_size);
+        }
+      else if (image_type == GTK_IMAGE_GICON)
+        {
+          GIcon * emblemed_icon = NULL;
+          gtk_image_get_gicon (
+            prev_img, &emblemed_icon, &icon_size);
+          g_return_if_fail (emblemed_icon);
+          GIcon * prev_icon = NULL;
+          if (G_IS_EMBLEMED_ICON (emblemed_icon))
+            {
+              prev_icon =
+                g_emblemed_icon_get_icon (
+                  G_EMBLEMED_ICON (emblemed_icon));
+            }
+          else if (G_IS_THEMED_ICON (emblemed_icon))
+            {
+              prev_icon = emblemed_icon;
+            }
+          else
+            {
+              g_return_if_reached ();
+            }
+
+          const char * const * icon_names =
+            g_themed_icon_get_names (
+              G_THEMED_ICON (prev_icon));
+          icon_name =  icon_names[0];
+        }
+      g_return_if_fail (icon_name);
+
+      GIcon * icon = g_themed_icon_new (icon_name);
+
+      /* add emblem if needed */
+      bool needs_emblem =
+        (has_collections &&
+         string_is_equal (
+           icon_name, COLLECTIONS_ICON)) ||
+        (has_protocols &&
+         string_is_equal (
+           icon_name, PROTOCOLS_ICON)) ||
+        (has_categories &&
+         string_is_equal (
+           icon_name, CATEGORIES_ICON));
+      if (needs_emblem)
+        {
+          GIcon * dot_icon =
+            g_themed_icon_new ("media-record");
+          GEmblem * emblem =
+            g_emblem_new (dot_icon);
+          icon =
+            g_emblemed_icon_new (icon, emblem);
+        }
+
+      /* set new icon */
+      GtkWidget * img =
+        gtk_image_new_from_gicon (
+          icon, icon_size);
+      gtk_widget_set_visible (img, true);
+      gtk_button_set_image (button, img);
+    }
+  g_list_free (children);
+}
 
 static void
 save_tree_view_selection (
@@ -521,7 +661,7 @@ show_plugin_context_menu (
   /* add to collection */
   menuitem =
     z_gtk_create_menu_item (
-      _("Add to collection"), "favorite",
+      _("Add to collection"), COLLECTIONS_ICON,
       false, NULL);
   gtk_widget_set_visible (
     GTK_WIDGET (menuitem), true);
@@ -1136,6 +1276,7 @@ on_selection_changed (
     (GDestroyNotify) gtk_tree_path_free);
 
   save_tree_view_selections (self);
+  update_stack_switcher_emblems (self);
 }
 
 static void
@@ -2006,11 +2147,11 @@ plugin_browser_widget_init (
   g_value_init (&iconval2, G_TYPE_STRING);
   g_value_init (&iconval3, G_TYPE_STRING);
   g_value_set_string (
-    &iconval1, "favorite");
+    &iconval1, COLLECTIONS_ICON);
   g_value_set_string(
-    &iconval2, "category");
+    &iconval2, CATEGORIES_ICON);
   g_value_set_string(
-    &iconval3, "protocol");
+    &iconval3, PROTOCOLS_ICON);
 
   gtk_container_child_set_property (
     GTK_CONTAINER (self->stack),
