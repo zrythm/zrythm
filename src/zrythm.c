f968fe2f8ccce20aa42ba851cbd197e4681f826e
Alexandros Theodotou <alex@zrythm.org>
2021-03-13 19:54:39 +0000

    fix blocking when attempting to connect ports

--- src/zrythm.c
+++ src/zrythm.c
@@ -354,15 +354,28 @@ zrythm_get_user_dir (
       return g_strdup (ZRYTHM->testing_dir);
     }
 
-  GSettings * settings =
-    g_settings_new (
-      GSETTINGS_ZRYTHM_PREFIX
-      ".preferences.general.paths");
-  g_return_val_if_fail (settings, NULL);
+  char * dir = NULL;
+  if (SETTINGS && S_P_GENERAL_PATHS)
+    {
+      dir =
+        g_settings_get_string (
+          S_P_GENERAL_PATHS, "zrythm-dir");
+    }
+  else
+    {
+      GSettings * settings =
+        g_settings_new (
+          GSETTINGS_ZRYTHM_PREFIX
+          ".preferences.general.paths");
+      g_return_val_if_fail (settings, NULL);
 
-  char * dir =
-    g_settings_get_string (
-      settings, "zrythm-dir");
+      dir =
+        g_settings_get_string (
+          settings, "zrythm-dir");
+
+      g_object_unref (settings);
+    }
+
   if (force_default || strlen (dir) == 0)
     {
       g_free (dir);
@@ -370,8 +383,6 @@ zrythm_get_user_dir (
         g_build_filename (
           g_get_user_data_dir (), "zrythm", NULL);
     }
-
-  g_object_unref (settings);
 
   return dir;
 }
