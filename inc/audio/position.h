zrythm/zrythm

commit cf32b74ea2f665e121d24f9f007d7e57f1a62abb

fix click and drag to move playhead not working in editor ruler
Fixes https://todo.sr.ht/~alextee/zrythm-bug/632.

inc/audio/position.h
@@ -56,6 +56,8 @@
 #define position_snap_simple(pos, sg) \
   position_snap (NULL, pos, NULL, NULL, sg)
 
+#define POSITION_MAX_BAR 80000
+
 /**
  * Whether the position starts on or after f1 and
  * before f2.

src/audio/position.c
@@ -112,7 +112,11 @@ position_set_to_bar (
   Position * self,
   int        bar)
 {
-  g_return_if_fail (TRANSPORT->ticks_per_bar > 0);
+  g_return_if_fail (
+    TRANSPORT->ticks_per_bar > 0
+    /* don't use INT_MAX, it results in a negative
+     * position */
+    && bar <= POSITION_MAX_BAR);
 
   position_init (self);
 

src/gui/widgets/editor_ruler.c
@@ -72,8 +72,8 @@ editor_ruler_on_drag_begin_no_marker_hit (
 void
 editor_ruler_on_drag_update (
   RulerWidget * self,
-  gdouble             offset_x,
-  gdouble             offset_y)
+  gdouble       offset_x,
+  gdouble       offset_y)
 {
   g_return_if_fail (self);
 
@@ -171,7 +171,7 @@ editor_ruler_on_drag_update (
                    timeline_end;
           position_init (&timeline_start);
           position_set_to_bar (
-            &timeline_end, INT_MAX);
+            &timeline_end, POSITION_MAX_BAR);
 
           /* if position is acceptable */
           if (position_is_after_or_equal (

src/gui/widgets/timeline_ruler.c
@@ -353,7 +353,7 @@ timeline_ruler_on_drag_update (
           position_init (&timeline_start);
           position_init (&timeline_end);
           position_set_to_bar (
-            &timeline_end, 80000);
+            &timeline_end, POSITION_MAX_BAR);
 
           /* convert px to position */
           ui_px_to_pos_timeline (
@@ -368,10 +368,10 @@ timeline_ruler_on_drag_update (
           if (self->target == RW_TARGET_PLAYHEAD)
             {
               /* if position is acceptable */
-              if (position_compare (
-                    &tmp, &timeline_start) >= 0 &&
-                  position_compare (
-                    &tmp, &timeline_end) <= 0)
+              if (position_is_after_or_equal (
+                    &tmp, &timeline_start) &&
+                  position_is_before_or_equal (
+                    &tmp, &timeline_end))
                 {
                   transport_move_playhead (
                     TRANSPORT, &tmp,

commit e078a50930fb42e54e9172006d1db0b0d72ce249

fix range action error (regression from cf32b74ea)

inc/audio/position.h
@@ -56,7 +56,7 @@
 #define position_snap_simple(pos, sg) \
   position_snap (NULL, pos, NULL, NULL, sg)
 
-#define POSITION_MAX_BAR 80000
+#define POSITION_MAX_BAR 160000
 
 /**
  * Whether the position starts on or after f1 and

src/actions/range_action.c
@@ -76,7 +76,7 @@ range_action_new (
 
   /* create selections for overlapping objects */
   Position inf;
-  position_set_to_bar (&inf, 160000);
+  position_set_to_bar (&inf, POSITION_MAX_BAR);
   self->sel_before =
     timeline_selections_new_for_range (
       start_pos, &inf, F_CLONE);