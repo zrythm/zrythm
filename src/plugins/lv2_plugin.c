zrythm/zrythm

commit 45a08fd7415132a0efee73fa6e4ae7612e7f3648

fix errors when plugins cannot be instantiated
* Fix segfault when requested LV2 plugin UI not found
* Fix error when saving project with uninstantiated plugins

src/plugins/lv2_plugin.c
@@ -130,7 +130,8 @@ typedef enum
 {
   Z_PLUGINS_LV2_PLUGIN_ERROR_CREATION_FAILED,
   Z_PLUGINS_LV2_PLUGIN_ERROR_INSTANTIATION_FAILED,
-} ZPluginsPluginError;
+  Z_PLUGINS_LV2_PLUGIN_ERROR_NO_UI,
+} ZPluginsLv2PluginError;
 
 #define Z_PLUGINS_LV2_PLUGIN_ERROR \
   z_plugins_lv2_plugin_error_quark ()
@@ -2255,7 +2256,8 @@ lv2_plugin_get_ui_binary_uri (
 bool
 lv2_plugin_is_ui_external (
   const char * uri,
-  const char * ui_uri)
+  const char * ui_uri,
+  GError **    error)
 {
   LilvNode * lv2_uri =
     lilv_new_uri (LILV_WORLD, uri);
@@ -2269,6 +2271,15 @@ lv2_plugin_is_ui_external (
     lilv_new_uri (LILV_WORLD, ui_uri);
   const LilvUI * ui =
     lilv_uis_get_by_uri (uis, ui_uri_node);
+  if (!ui)
+    {
+      g_set_error (
+        error, Z_PLUGINS_LV2_PLUGIN_ERROR,
+        Z_PLUGINS_LV2_PLUGIN_ERROR_NO_UI,
+        _("Could not find UI by URI <%s>"),
+        ui_uri);
+      return false;
+    }
 
   const LilvNodes * ui_classes =
     lilv_ui_get_classes (ui);
@@ -2560,7 +2571,8 @@ lv2_plugin_instantiate (
   bool         project,
   bool         use_state_file,
   char *       preset_uri,
-  LilvState *  state)
+  LilvState *  state,
+  GError **    error)
 {
   g_message (
     "Instantiating... uri: %s, project: %d, "
@@ -2616,9 +2628,9 @@ lv2_plugin_instantiate (
     g_dir_make_tmp ("lv2_self_XXXXXX", &err);
   if (!templ)
     {
-      g_warning (
-        "LV2 plugin instantiation failed: %s",
-        err->message);
+      PROPAGATE_PREFIXED_ERROR (
+        error, err, "%s",
+        _("Failed to make temporary dir"));
       return -1;
     }
   self->temp_dir =
@@ -2677,11 +2689,11 @@ lv2_plugin_instantiate (
               state_file_path);
           if (!state)
             {
-              g_critical (
-                "Failed to load state from %s",
+              PROPAGATE_PREFIXED_ERROR (
+                error, err,
+                _("Failed to load state from %s"),
                 state_file_path);
-              return
-                ERR_FAILED_TO_LOAD_STATE_FROM_FILE;
+              return -1;
             }
           g_free (state_file_path);
 
@@ -2709,21 +2721,11 @@ lv2_plugin_instantiate (
               LILV_PLUGINS, lv2_uri);
           if (!self->lilv_plugin)
             {
-              if (ZRYTHM_HAVE_UI)
-                {
-                  char * msg =
-                    g_strdup_printf (
-                      _("Failed to find plugin "
-                      "with URI: %s"),
-                      lv2_uri_str);
-                  g_warning ("%s", msg);
-                  if (ZRYTHM_HAVE_UI)
-                    {
-                      ui_show_error_message (
-                        MAIN_WINDOW, msg);
-                    }
-                  g_free (msg);
-                }
+              PROPAGATE_PREFIXED_ERROR (
+                error, err,
+                _("Failed to find plugin with URI "
+                "<%s>"),
+                lv2_uri_str);
               lilv_node_free (lv2_uri);
               return -1;
             }
@@ -2901,8 +2903,18 @@ lv2_plugin_instantiate (
   if (ui_uri)
     {
       /* set whether the UI is external */
-      if (lv2_plugin_is_ui_external (
-            pl_uri, ui_uri))
+      bool is_ui_external =
+        lv2_plugin_is_ui_external (
+          pl_uri, ui_uri, &err);
+      if (err)
+        {
+          PROPAGATE_PREFIXED_ERROR (
+            error, err, "%s",
+            _("Failed checking whether lv2 plugin "
+            "UI is external"));
+          return -1;
+        }
+      else if (is_ui_external)
         {
           self->has_external_ui = true;
         }

src/utils/error.c
@@ -24,27 +24,6 @@
 
 #include <glib/gi18n.h>
 
-const char *
-error_code_get_message (
-  ErrorCode err_code)
-{
-  switch (err_code)
-    {
-    case ERR_PLUGIN_INSTANTIATION_FAILED:
-      return
-        _("Plugin instantiation failed. "
-          "See the logs for details");
-    case ERR_OBJECT_IS_NULL:
-      return
-        _("Object is null. "
-          "See the logs for details");
-    default:
-      break;
-    }
-
-  g_return_val_if_reached (_("Unknown error"));
-}
-
 /**
  * Only to be called by HANDLE_ERROR macro.
  */

src/plugins/plugin.c
@@ -230,8 +230,10 @@ plugin_init_loaded (
     {
       bool was_enabled =
         plugin_is_enabled (self, false);
+      GError * err = NULL;
       int ret =
-        plugin_instantiate (self, project, NULL);
+        plugin_instantiate (
+          self, project, NULL, &err);
       if (ret == 0)
         {
           plugin_activate (self, true);
@@ -242,18 +244,11 @@ plugin_init_loaded (
       else
         {
           /* disable plugin, instantiation failed */
-          char * msg =
-            g_strdup_printf (
-              _("Instantiation failed for "
-              "plugin '%s'. Disabling..."),
-              self->setting->descr->name);
-          g_warning ("%s", msg);
-          if (ZRYTHM_HAVE_UI)
-            {
-              ui_show_error_message (
-                MAIN_WINDOW, msg);
-            }
-          g_free (msg);
+          HANDLE_ERROR (
+            err,
+            _("Instantiation failed for "
+            "plugin '%s'. Disabling..."),
+            self->setting->descr->name);
           self->instantiation_failed = true;
         }
     }
@@ -565,10 +560,14 @@ plugin_new_from_setting (
 #ifdef HAVE_CARLA
   if (setting->open_with_carla)
     {
-      carla_native_plugin_new_from_setting (self);
+      GError * err = NULL;
+      carla_native_plugin_new_from_setting (
+        self, &err);
       if (!self->carla)
         {
-          g_warning ("failed to create plugin");
+          PROPAGATE_PREFIXED_ERROR (
+            error, err, "%s",
+            _("Failed to get Carla plugin"));
           return NULL;
         }
     }
@@ -1164,8 +1163,7 @@ plugin_activate (
   Plugin * pl,
   bool     activate)
 {
-  g_return_val_if_fail (
-    IS_PLUGIN (pl), ERR_OBJECT_IS_NULL);
+  g_return_val_if_fail (IS_PLUGIN (pl), -1);
 
   if ((pl->activated && activate) ||
       (!pl->activated && !activate))
@@ -1635,13 +1633,15 @@ int
 plugin_instantiate (
   Plugin *    pl,
   bool        project,
-  LilvState * state)
+  LilvState * state,
+  GError **   error)
 {
   g_return_val_if_fail (
     pl->setting && pl->setting->descr, -1);
 
-  g_message ("Instantiating %s...",
-             pl->setting->descr->name);
+  g_message (
+    "Instantiating %s...",
+    pl->setting->descr->name);
 
   plugin_set_ui_refresh_rate (pl);
 
@@ -1654,14 +1654,17 @@ plugin_instantiate (
   if (pl->setting->open_with_carla)
     {
 #ifdef HAVE_CARLA
+      GError * err = NULL;
       int ret =
         carla_native_plugin_instantiate (
           pl->carla, !PROJECT->loaded,
-          pl->state_dir ? true : false);
+          pl->state_dir ? true : false,
+          &err);
       if (ret != 0)
         {
-          g_warning (
-            "carla plugin instantiation failed");
+          PROPAGATE_PREFIXED_ERROR (
+            error, err, "%s",
+            _("Carla plugin instantiation failed"));
           return -1;
         }
 
@@ -1679,20 +1682,26 @@ plugin_instantiate (
         case PROT_LV2:
           {
             pl->lv2->plugin = pl;
-            if (lv2_plugin_instantiate (
-                  pl->lv2, project,
-                  pl->state_dir ? true : false,
-                  NULL, state))
+            GError * err = NULL;
+            int ret =
+              lv2_plugin_instantiate (
+                pl->lv2, project,
+                pl->state_dir ? true : false,
+                NULL, state, &err);
+            if (ret != 0)
               {
-                g_warning (
-                  "lv2 instantiate failed");
+                PROPAGATE_PREFIXED_ERROR (
+                  error, err, "%s",
+                  _("LV2 plugin instantiation "
+                  "failed"));
                 return -1;
               }
             else
               {
                 pl->instantiated = true;
               }
-            g_warn_if_fail (pl->lv2->instance);
+            g_return_val_if_fail (
+              pl->lv2->instance, -1);
 
             if (!pl->state_dir)
               {
@@ -2138,17 +2147,17 @@ plugin_clone (
 
       /* instantiate */
       int ret =
-        plugin_instantiate (clone, false, NULL);
+        plugin_instantiate (
+          clone, false, NULL, &err);
       if (ret != 0)
         {
-          g_warn_if_reached ();
           object_free_w_func_and_null (
             plugin_free, clone);
-          g_set_error (
-            error, Z_PLUGINS_PLUGIN_ERROR,
-            Z_PLUGINS_PLUGIN_ERROR_INSTANTIATION_FAILED,
-            "Failed to instantiate cloned "
-            "plugin %s", buf);
+          PROPAGATE_PREFIXED_ERROR (
+            error, err,
+            _("Failed to instantiate cloned "
+            "plugin %s"),
+            buf);
           return NULL;
         }
 
@@ -2159,15 +2168,14 @@ plugin_clone (
         {
           ret =
             plugin_instantiate (
-              pl, src_is_project, NULL);
+              pl, src_is_project, NULL, &err);
           if (ret != 0)
             {
-              g_warn_if_reached ();
-              g_set_error (
-                error, Z_PLUGINS_PLUGIN_ERROR,
-                Z_PLUGINS_PLUGIN_ERROR_INSTANTIATION_FAILED,
-                "Failed to reinstantiate source "
-                "plugin %s", buf);
+              PROPAGATE_PREFIXED_ERROR (
+                error, err,
+                _("Failed to instantiate source "
+                "plugin"),
+                buf);
               return NULL;
             }
           g_return_val_if_fail (
@@ -2212,11 +2220,19 @@ plugin_clone (
               g_message (
                 "source plugin not instantiated, "
                 "instantiating it...");
+              GError * err = NULL;
               int ret =
                 plugin_instantiate (
-                  pl, src_is_project, NULL);
-              g_return_val_if_fail (
-                ret == 0, NULL);
+                  pl, src_is_project, NULL, &err);
+              if (ret != 0)
+                {
+                  PROPAGATE_PREFIXED_ERROR (
+                    error, err,
+                    _("Failed to instantiate "
+                    "source plugin"),
+                    buf);
+                  return NULL;
+                }
             }
           g_return_val_if_fail (
             pl->instantiated &&
@@ -2246,8 +2262,15 @@ plugin_clone (
           /* instantiate using the state */
           int ret =
             plugin_instantiate (
-              clone, false, state);
-          g_return_val_if_fail (!ret, NULL);
+              clone, false, state, &err);
+          if (ret != 0)
+            {
+              PROPAGATE_PREFIXED_ERROR (
+                error, err, "%s",
+                _("Failed to instantiate cloned "
+                "plugin"));
+              return NULL;
+            }
 
           /* verify */
           g_return_val_if_fail (

inc/audio/audio_function.h
@@ -123,14 +123,24 @@ audio_function_get_plugin_uri_for_type (
 /**
  * Applies the given action to the given selections.
  *
+ * This will save a file in the pool and store the
+ * pool ID in the selections.
+ *
  * @param sel Selections to edit.
- * @param type Function type.
+ * @param type Function type. If invalid is passed,
+ *   this will simply add the audio file in the pool
+ *   for the unchanged audio material (used in
+ *   audio selection actions for the selections
+ *   before the change).
+ *
+ * @return Non-zero if error.
  */
 int
 audio_function_apply (
   ArrangerSelections * sel,
   AudioFunctionType    type,
-  const char *         uri);
+  const char *         uri,
+  GError **            error);
 
 /**
  * @}

inc/plugins/carla_native_plugin.h
@@ -104,10 +104,11 @@ carla_native_plugin_init_loaded (
  *
  * @return Non-zero if fail.
  */
-NONNULL
+NONNULL_ARGS (1)
 int
 carla_native_plugin_new_from_setting (
-  Plugin * plugin);
+  Plugin *  plugin,
+  GError ** error);
 
 /**
  * Returns a filled in descriptor from the
@@ -161,12 +162,13 @@ carla_native_plugin_populate_banks (
  *
  * @return 0 if no errors, non-zero if errors.
  */
-NONNULL
+NONNULL_ARGS (1)
 int
 carla_native_plugin_instantiate (
   CarlaNativePlugin * self,
   bool                loading,
-  bool                use_state_file);
+  bool                use_state_file,
+  GError **           error);
 
 NONNULL
 char *

inc/plugins/lv2_plugin.h
@@ -412,13 +412,15 @@ lv2_plugin_new_from_uri (
  *
  * @return 0 if OK, non-zero if error.
  */
+NONNULL_ARGS (1)
 int
 lv2_plugin_instantiate (
   Lv2Plugin *  self,
   bool         project,
   bool         use_state_file,
   char *       preset_uri,
-  LilvState *  state);
+  LilvState *  state,
+  GError **    error);
 
 /**
  * Creates a new LV2 plugin using the given Plugin
@@ -572,7 +574,8 @@ NONNULL
 bool
 lv2_plugin_is_ui_external (
   const char * uri,
-  const char * ui_uri);
+  const char * ui_uri,
+  GError **    error);
 
 /**
  * Ported from Lv2Control.

inc/plugins/plugin.h
@@ -672,12 +672,13 @@ plugin_prepare_process (
  * @param project Whether this is a project plugin
  *   (as opposed to a clone used in actions).
  */
-__attribute__ ((nonnull (1)))
+NONNULL_ARGS (1)
 int
 plugin_instantiate (
   Plugin *    self,
   bool        project,
-  LilvState * state);
+  LilvState * state,
+  GError **   error);
 
 /**
  * Sets the track and track_pos on the plugin.

inc/utils/error.h
@@ -28,31 +28,6 @@
 
 #include <glib.h>
 
-/* FIXME delete start */
-typedef enum ErrorDomain
-{
-  ERROR_DOMAIN_GLAREA,
-} ErrorDomain;
-
-typedef enum ErrorCode
-{
-  ERR_PLUGIN_INSTANTIATION_FAILED = 1,
-  ERR_OBJECT_IS_NULL,
-  ERR_PORT_MAGIC_FAILED,
-  ERR_FAILED_TO_LOAD_STATE_FROM_FILE,
-  ERROR_SHADER_COMPILATION,
-  ERROR_SHADER_LINK,
-} ErrorCode;
-
-/**
- * Return the message corresponding to the given
- * error code.
- */
-const char *
-error_code_get_message (
-  ErrorCode err_code);
-/* FIXME delete end */
-
 /**
  * Only to be called by HANDLE_ERROR macro.
  */

commit 5457b420a502be3d432b4efd0ebefb6bb5d6ea21

fix broken plugin test (regression from 45a08fd74)

src/plugins/lv2_plugin.c
@@ -131,6 +131,7 @@ typedef enum
   Z_PLUGINS_LV2_PLUGIN_ERROR_CREATION_FAILED,
   Z_PLUGINS_LV2_PLUGIN_ERROR_INSTANTIATION_FAILED,
   Z_PLUGINS_LV2_PLUGIN_ERROR_NO_UI,
+  Z_PLUGINS_LV2_PLUGIN_ERROR_CANNOT_FIND_URI,
 } ZPluginsLv2PluginError;
 
 #define Z_PLUGINS_LV2_PLUGIN_ERROR \
@@ -2721,8 +2722,10 @@ lv2_plugin_instantiate (
               LILV_PLUGINS, lv2_uri);
           if (!self->lilv_plugin)
             {
-              PROPAGATE_PREFIXED_ERROR (
-                error, err,
+              g_set_error (
+                error,
+                Z_PLUGINS_LV2_PLUGIN_ERROR,
+                Z_PLUGINS_LV2_PLUGIN_ERROR_CANNOT_FIND_URI,
                 _("Failed to find plugin with URI "
                 "<%s>"),
                 lv2_uri_str);

src/utils/error.c
@@ -65,6 +65,8 @@ error_propagate_prefixed_prv (
   const char * format,
   ...)
 {
+  g_return_if_fail (err != NULL);
+
   va_list args;
   va_start (args, format);
 

tests/plugins/plugin.c
@@ -53,9 +53,6 @@ _test_loading_non_existing_plugin (
     G_LOG_LEVEL_WARNING;
   g_test_expect_message (
     G_LOG_DOMAIN, G_LOG_LEVEL_WARNING,
-    "*lv2 instantiate failed*");
-  g_test_expect_message (
-    G_LOG_DOMAIN, G_LOG_LEVEL_WARNING,
     "*Instantiation failed for plugin *");
   test_project_save_and_reload ();
 