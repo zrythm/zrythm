zrythm/zrythm

commit 71daceb1c42683a2b5fa0af8cbd91f4cbf947b24

resize-loop when attempting to resize only and have looped objects
Switch to resize-loop when attempting to resize-only a selection that
contains a mix of looped and unlooped objects (fixes crash).

Fixes https://todo.sr.ht/~alextee/zrythm-bug/614.

src/gui/backend/arranger_selections.c
@@ -1989,6 +1989,78 @@ arranger_selections_contains_undeletable_object (
 }
 
 /**
+ * Checks whether an object matches the given
+ * parameters.
+ *
+ * If a parameter should be checked, the has_*
+ * argument must be true and the corresponding
+ * argument must have the value to be checked
+ * against.
+ */
+bool
+arranger_selections_contains_object_with_property (
+  ArrangerSelections *       self,
+  ArrangerSelectionsProperty property,
+  bool                       value)
+{
+  int num_objs;
+  ArrangerObject ** objs =
+    arranger_selections_get_all_objects (
+      self, &num_objs);
+
+#define CHECK_PROP(x) \
+  (property == ARRANGER_SELECTIONS_PROPERTY_##x)
+
+  bool ret = false;
+  for (int i = 0; i < num_objs; i++)
+    {
+      ArrangerObject * cur_obj = objs[i];
+
+      if (CHECK_PROP (HAS_LENGTH)
+          &&
+          arranger_object_type_has_length (
+            cur_obj->type) == value)
+        {
+          ret = true;
+          break;
+        }
+
+      int num_loops =
+        arranger_object_get_num_loops (
+          cur_obj, false);
+      if (CHECK_PROP (HAS_LOOPED)
+          && (num_loops > 0) == value)
+        {
+          ret = true;
+          break;
+        }
+
+      if (CHECK_PROP (CAN_LOOP)
+          &&
+          arranger_object_type_can_loop (
+            cur_obj->type) == value)
+        {
+          ret = true;
+          break;
+        }
+
+      if (CHECK_PROP (CAN_FADE)
+          &&
+          arranger_object_can_fade (cur_obj)
+            == value)
+        {
+          ret = true;
+          break;
+        }
+    }
+
+#undef CHECK_PROP
+
+  free (objs);
+  return ret;
+}
+
+/**
  * Removes the arranger object from the selections.
  */
 void

src/actions/arranger_selections.c
@@ -720,6 +720,60 @@ arranger_selections_action_new_resize (
   ArrangerSelectionsActionResizeType type,
   const double                       ticks)
 {
+  /* validate */
+  bool have_unresizable =
+    arranger_selections_contains_object_with_property (
+      sel,
+      ARRANGER_SELECTIONS_PROPERTY_HAS_LENGTH,
+      false);
+  g_return_val_if_fail (!have_unresizable, NULL);
+
+  bool have_looped =
+    arranger_selections_contains_object_with_property (
+      sel,
+      ARRANGER_SELECTIONS_PROPERTY_HAS_LOOPED,
+      true);
+  if (have_looped
+      &&
+      (type
+         == ARRANGER_SELECTIONS_ACTION_RESIZE_L
+       ||
+       type
+         == ARRANGER_SELECTIONS_ACTION_RESIZE_R
+       ||
+       type
+         == ARRANGER_SELECTIONS_ACTION_STRETCH_L
+       ||
+       type
+         == ARRANGER_SELECTIONS_ACTION_STRETCH_R))
+    {
+      g_critical (
+        "cannot perform %s resize - selections "
+        "contain looped objects",
+        arranger_selections_action_resize_type_strings[type].str);
+      return NULL;
+    }
+
+  bool have_unloopable =
+    arranger_selections_contains_object_with_property (
+      sel,
+      ARRANGER_SELECTIONS_PROPERTY_CAN_LOOP,
+      false);
+  if (have_unloopable
+      &&
+      (type
+         == ARRANGER_SELECTIONS_ACTION_RESIZE_L_LOOP
+       ||
+       type
+         == ARRANGER_SELECTIONS_ACTION_RESIZE_R_LOOP))
+    {
+      g_critical (
+        "cannot perform %s resize - selections "
+        "contain unloopable objects",
+        arranger_selections_action_resize_type_strings[type].str);
+      return NULL;
+    }
+
   ArrangerSelectionsAction * self =
     _create_action (sel);
   self->type = AS_ACTION_RESIZE;

inc/gui/backend/arranger_selections.h
@@ -64,16 +64,6 @@ typedef enum ArrangerSelectionsType
   ARRANGER_SELECTIONS_TYPE_AUDIO,
 } ArrangerSelectionsType;
 
-typedef struct ArrangerSelections
-{
-  int                    schema_version;
-
-  /** Type of selections. */
-  ArrangerSelectionsType type;
-
-  int                    magic;
-} ArrangerSelections;
-
 static const cyaml_strval_t
 arranger_selections_type_strings[] =
 {
@@ -90,6 +80,16 @@ arranger_selections_type_strings[] =
   { "Audio",
     ARRANGER_SELECTIONS_TYPE_AUDIO   },
 };
+
+typedef struct ArrangerSelections
+{
+  int                    schema_version;
+
+  /** Type of selections. */
+  ArrangerSelectionsType type;
+
+  int                    magic;
+} ArrangerSelections;
 
 static const cyaml_schema_field_t
 arranger_selections_fields_schema[] =
@@ -109,6 +109,14 @@ arranger_selections_schema = {
     ArrangerSelections,
     arranger_selections_fields_schema),
 };
+
+typedef enum ArrangerSelectionsProperty
+{
+  ARRANGER_SELECTIONS_PROPERTY_HAS_LENGTH,
+  ARRANGER_SELECTIONS_PROPERTY_CAN_LOOP,
+  ARRANGER_SELECTIONS_PROPERTY_HAS_LOOPED,
+  ARRANGER_SELECTIONS_PROPERTY_CAN_FADE,
+} ArrangerSelectionsProperty;
 
 static inline ArrangerSelections *
 arranger_selections_cast (void * sel)
@@ -403,6 +411,22 @@ NONNULL
 bool
 arranger_selections_contains_undeletable_object (
   ArrangerSelections * self);
+
+/**
+ * Checks whether an object matches the given
+ * parameters.
+ *
+ * If a parameter should be checked, the has_*
+ * argument must be true and the corresponding
+ * argument must have the value to be checked
+ * against.
+ */
+NONNULL
+bool
+arranger_selections_contains_object_with_property (
+  ArrangerSelections *       self,
+  ArrangerSelectionsProperty property,
+  bool                       value);
 
 /**
  * Removes the arranger object from the selections.

src/gui/widgets/arranger.c
@@ -2416,49 +2416,49 @@ on_drag_begin_handle_hit_object (
     {
       ArrangerSelections * sel =
         arranger_widget_get_selections (self);
-      int num_objs;
-      ArrangerObject ** objs =
-        arranger_selections_get_all_objects (
-          sel, &num_objs);
-      bool have_looped = false;
-      bool have_unloopable = false;
-      for (int i = 0; i < num_objs; i++)
+
+      bool have_unresizable =
+        arranger_selections_contains_object_with_property (
+          sel,
+          ARRANGER_SELECTIONS_PROPERTY_HAS_LENGTH,
+          false);
+      if (have_unresizable)
         {
-          ArrangerObject * cur_obj = objs[i];
-          if (!arranger_object_type_has_length (
-                 cur_obj->type))
-            {
-              is_resize_l = false;
-              is_resize_r = false;
-              is_resize_loop = false;
-              break;
-
-              int num_loops =
-                arranger_object_get_num_loops (
-                  cur_obj, false);
-              if (num_loops > 0)
-                have_looped = true;
-
-              if (!arranger_object_type_can_loop (
-                    cur_obj->type))
-                have_unloopable = true;
-            }
+          ui_show_message_printf (
+            MAIN_WINDOW, GTK_MESSAGE_WARNING,
+            "%s",
+            _("Cannot resize because the "
+            "selection contains objects "
+            "without length"));
+          return false;
         }
 
-      if (!is_resize_loop && have_looped)
+      bool have_looped =
+        arranger_selections_contains_object_with_property (
+          sel,
+          ARRANGER_SELECTIONS_PROPERTY_HAS_LOOPED,
+          true);
+      if ((is_resize_l || is_resize_r)
+          && !is_resize_loop && have_looped)
         {
+          bool have_unloopable =
+            arranger_selections_contains_object_with_property (
+              sel,
+              ARRANGER_SELECTIONS_PROPERTY_CAN_LOOP,
+              false);
           if (have_unloopable)
             {
               /* cancel resize since we have
                * a looped object mixed with
                * unloopable objects in the
                * selection */
-              g_debug (
-                "cancel resize - "
-                "have looped object mixed with "
-                "unloopable objects");
-              is_resize_l = false;
-              is_resize_r = false;
+              ui_show_message_printf (
+                MAIN_WINDOW, GTK_MESSAGE_WARNING,
+                "%s",
+                _("Cannot resize because the "
+                "selection contains a mix of "
+                "looped and unloopable objects"));
+              return false;
             }
           else
             {
@@ -2479,21 +2479,18 @@ on_drag_begin_handle_hit_object (
     {
       ArrangerSelections * sel =
         arranger_widget_get_selections (self);
-      int num_objs;
-      ArrangerObject ** objs =
-        arranger_selections_get_all_objects (
-          sel, &num_objs);
-      for (int i = 0; i < num_objs; i++)
+      bool have_unfadeable =
+        arranger_selections_contains_object_with_property (
+          sel,
+          ARRANGER_SELECTIONS_PROPERTY_CAN_FADE,
+          false);
+      if (have_unfadeable)
         {
-          ArrangerObject * cur_obj = objs[i];
-          if (!arranger_object_can_fade (cur_obj))
-            {
-              is_fade_in_point = false;
-              is_fade_in_outer = false;
-              is_fade_out_point = false;
-              is_fade_out_outer = false;
-              break;
-            }
+          /* don't fade */
+          is_fade_in_point = false;
+          is_fade_in_outer = false;
+          is_fade_out_point = false;
+          is_fade_out_outer = false;
         }
     }
 
@@ -3440,10 +3437,10 @@ drag_update (
             {
               int ret =
                 timeline_arranger_widget_snap_regions_r (
-                  self, &self->curr_pos, 1);
+                  self, &self->curr_pos, F_DRY_RUN);
               if (!ret)
                 timeline_arranger_widget_snap_regions_r (
-                  self, &self->curr_pos, 0);
+                  self, &self->curr_pos, F_NOT_DRY_RUN);
             }
         }
       break;

src/gui/widgets/timeline_arranger.c
@@ -771,7 +771,7 @@ timeline_arranger_widget_snap_regions_l (
 /**
  * Snaps the region's end point.
  *
- * @param new_end_pos Position to snap to.
+ * @param new_end_pos New end position to snap to.
  * @parram dry_run Don't resize notes; just check
  *   if the resize is allowed (check if invalid
  *   resizes will happen)
@@ -779,7 +779,7 @@ timeline_arranger_widget_snap_regions_l (
  * @return 0 if the operation was successful,
  *   nonzero otherwise.
  */
-static inline int
+static int
 snap_region_r (
   ArrangerWidget * self,
   ZRegion *        region,
@@ -795,9 +795,9 @@ snap_region_r (
   else if ACTION_IS (STRETCHING_R)
     type = ARRANGER_OBJECT_RESIZE_STRETCH;
 
-  if (SNAP_GRID_ANY_SNAP (self->snap_grid) &&
-        !self->shift_held &&
-        type != ARRANGER_OBJECT_RESIZE_FADE)
+  if (SNAP_GRID_ANY_SNAP (self->snap_grid)
+      && !self->shift_held
+      && type != ARRANGER_OBJECT_RESIZE_FADE)
     {
       Track * track =
         arranger_object_get_track (

commit 728900c1b0e53e738ee7fed2d61e986df7723118

fix error when attempting to resize MIDI notes from the left
Regression from 71daceb1c.

src/gui/backend/arranger_selections.c
@@ -2026,14 +2026,16 @@ arranger_selections_contains_object_with_property (
           break;
         }
 
-      int num_loops =
-        arranger_object_get_num_loops (
-          cur_obj, false);
-      if (CHECK_PROP (HAS_LOOPED)
-          && (num_loops > 0) == value)
+      if (CHECK_PROP (HAS_LOOPED))
         {
-          ret = true;
-          break;
+          int num_loops =
+            arranger_object_get_num_loops (
+              cur_obj, false);
+          if ((num_loops > 0) == value)
+            {
+              ret = true;
+              break;
+            }
         }
 
       if (CHECK_PROP (CAN_LOOP)

tests/gui/backend/arranger_selections.c
@@ -118,6 +118,54 @@ test_get_last_object (void)
   test_helper_zrythm_cleanup ();
 }
 
+static void
+test_contains_object_with_property (void)
+{
+  test_helper_zrythm_init ();
+
+  Track * track =
+    track_create_empty_with_action (
+      TRACK_TYPE_MIDI);
+
+  Position p1, p2;
+  position_set_to_bar (&p1, 3);
+  position_set_to_bar (&p2, 4);
+  ZRegion * r =
+    midi_region_new (
+      &p1, &p2, track->pos, 0, 0);
+  track_add_region (
+    track, r, NULL, 0, F_GEN_NAME,
+    F_NO_PUBLISH_EVENTS);
+
+  position_from_frames (&p1, -40000);
+  position_from_frames (&p2, -4000);
+  MidiNote * mn =
+    midi_note_new (
+      &r->id, &p1, &p2, 60, 60);
+  ArrangerObject * mn_obj = (ArrangerObject *) mn;
+  midi_region_add_midi_note (
+    r, mn, F_NO_PUBLISH_EVENTS);
+
+  arranger_object_select (
+    mn_obj, F_SELECT, F_NO_APPEND,
+    F_NO_PUBLISH_EVENTS);
+
+  bool ret =
+    arranger_selections_contains_object_with_property (
+      (ArrangerSelections *) MA_SELECTIONS,
+      ARRANGER_SELECTIONS_PROPERTY_HAS_LENGTH,
+      true);
+  g_assert_true (ret);
+  ret =
+    arranger_selections_contains_object_with_property (
+      (ArrangerSelections *) MA_SELECTIONS,
+      ARRANGER_SELECTIONS_PROPERTY_HAS_LENGTH,
+      false);
+  g_assert_false (ret);
+
+  test_helper_zrythm_cleanup ();
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -125,6 +173,9 @@ main (int argc, char *argv[])
 
 #define TEST_PREFIX "/gui/backend/arranger selections/"
 
+  g_test_add_func (
+    TEST_PREFIX "test contains object with property",
+    (GTestFunc) test_contains_object_with_property);
   g_test_add_func (
     TEST_PREFIX "test get last object",
     (GTestFunc) test_get_last_object);