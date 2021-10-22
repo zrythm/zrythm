zrythm/zrythm

commit 1e45d1046da53c2a384c5ffee6c8feee859109a0

don't allow auto-disarm while recording
* Fix error when clicking on a track while recording (reverts 58d02fe41)
* Fix error when recording automation (too many UI events)

src/audio/track.c
@@ -880,17 +880,6 @@ track_set_recording (
     "%s: setting recording %d (fire events: %d)",
     track->name, recording, fire_events);
 
-  /* return if currently recording
-   * TODO in the future make it possible to toggle
-   * track recording while transport is recording */
-  if (TRANSPORT_IS_RECORDING
-      && TRANSPORT_IS_ROLLING)
-    {
-      g_message (
-        "already recording, skipping %s", __func__);
-      return;
-    }
-
   Channel * channel =
     track_get_channel (track);
 

src/gui/backend/event_manager.c
@@ -1146,25 +1146,29 @@ on_plugin_visibility_changed (Plugin * pl)
 static inline void
 clean_duplicates_and_copy (
   EventManager * self,
-  ZEvent **      events,
+  GPtrArray *    events_arr,
   int *          num_events)
 {
   MPMCQueue * q = self->mqueue;
   ZEvent * event;
   *num_events = 0;
-  int i, already_exists = 0;;
 
   /* only add events once to new array while
    * popping */
   while (event_queue_dequeue_event (
            q, &event))
     {
-      already_exists = 0;
+      bool already_exists = false;
 
-      for (i = 0; i < *num_events; i++)
-        if (event->type == events[i]->type &&
-            event->arg == events[i]->arg)
-          already_exists = 1;
+      for (int i = 0; i < *num_events; i++)
+        {
+          ZEvent * cur_event =
+            (ZEvent *)
+            g_ptr_array_index (events_arr, i);
+          if (event->type == cur_event->type &&
+              event->arg == cur_event->arg)
+            already_exists = true;
+        }
 
       if (already_exists)
         {
@@ -1173,8 +1177,8 @@ clean_duplicates_and_copy (
         }
       else
         {
-          array_append (
-            events, (*num_events), event);
+          g_ptr_array_add (events_arr, event);
+          (*num_events) = *num_events + 1;
         }
     }
 }
@@ -1976,11 +1980,10 @@ process_events (void * data)
 {
   EventManager * self = (EventManager *) data;
 
-  ZEvent * events[100];
   ZEvent * ev;
   int num_events = 0, i;
   clean_duplicates_and_copy (
-    self, events, &num_events);
+    self, self->events_arr, &num_events);
 
   /*g_message ("starting processing");*/
   for (i = 0; i < num_events; i++)
@@ -1988,10 +1991,13 @@ process_events (void * data)
       if (i > 30)
         {
           g_message (
-            "more than 30 UI events processed!");
+            "more than 30 UI events processed "
+            "(%d)!", i);
         }
 
-      ev = events[i];
+      ev =
+        (ZEvent *)
+        g_ptr_array_index (self->events_arr, i);
 
       if (!ZRYTHM_HAVE_UI)
         {
@@ -2066,6 +2072,9 @@ event_manager_new (void)
     self->mqueue,
     (size_t)
     EVENT_MANAGER_MAX_EVENTS * sizeof (ZEvent *));
+
+  self->events_arr =
+    g_ptr_array_sized_new (200);
 
   return self;
 }
@@ -2147,6 +2156,8 @@ event_manager_free (
     object_pool_free, self->obj_pool);
   object_free_w_func_and_null (
     mpmc_queue_free, self->mqueue);
+  object_free_w_func_and_null (
+    g_ptr_array_unref, self->events_arr);
 
   object_zero_and_free (self);
 

inc/audio/recording_manager.h
@@ -27,6 +27,7 @@
 #define __AUDIO_RECORDING_MANAGER_H__
 
 #include "utils/types.h"
+#include "zix/sem.h"
 
 typedef struct ObjectPool ObjectPool;
 typedef struct TrackProcessor TrackProcessor;
@@ -68,6 +69,9 @@ typedef struct RecordingManager
    */
   RegionIdentifier   recorded_ids[8000];
   int                num_recorded_ids;
+
+  bool               currently_processing;
+  ZixSem             processing_sem;
 
   bool               freeing;
 } RecordingManager;

inc/gui/backend/event_manager.h
@@ -64,6 +64,9 @@ typedef struct EventManager
   /** A soft recalculation of the routing graph
    * is pending. */
   bool               pending_soft_recalc;
+
+  /** Events array to use during processing. */
+  GPtrArray *        events_arr;
 } EventManager;
 
 #define EVENT_MANAGER (ZRYTHM->event_manager)

inc/utils/debug.h
@@ -38,6 +38,14 @@
 #define z_return_if_fail_cmp(a,comparator,b) \
   z_return_val_if_fail_cmp (a, comparator, b, )
 
+#define z_warn_if_fail_cmp(a,comparator,b) \
+  if (!(G_LIKELY (a comparator b))) \
+    { \
+      g_warning ( \
+        "Assertion failed: %s (%ld) %s %s (%ld)", \
+        #a, (long) a, #comparator, #b, (long) b); \
+    }
+
 /**
  * @}
  */

src/audio/engine.c
@@ -1131,9 +1131,15 @@ engine_wait_for_pause (
 
   if (PROJECT->loaded)
     {
+#if 0
       /* process all recording events */
+      zix_sem_wait (
+        &RECORDING_MANAGER->processing_sem);
       recording_manager_process_events (
         RECORDING_MANAGER);
+      zix_sem_post (
+        &RECORDING_MANAGER->processing_sem);
+#endif
 
       /* run one more time to flush panic
        * messages */

src/audio/recording_manager.c
@@ -1549,7 +1549,12 @@ recording_manager_process_events (
   RecordingManager * self)
 {
   /*gint64 curr_time = g_get_monotonic_time ();*/
-  /*g_message ("starting processing");*/
+  /*g_message ("~~~~~~~~~~~~~~~~starting processing");*/
+  zix_sem_wait (&self->processing_sem);
+  int i = 0;
+  g_return_val_if_fail (
+    !self->currently_processing, G_SOURCE_REMOVE);
+  self->currently_processing = true;
   RecordingEvent * ev;
   while (recording_event_queue_dequeue_event (
            self->event_queue, &ev))
@@ -1558,6 +1563,7 @@ recording_manager_process_events (
         {
           goto return_to_pool;
         }
+      i++;
 
       /*g_message ("event type %d", ev->type);*/
 
@@ -1675,7 +1681,10 @@ return_to_pool:
       object_pool_return (
         self->event_obj_pool, ev);
     }
-  /*g_message ("processed %d events", i);*/
+  /*g_message ("~~~~~~~~~~~processed %d events", i);*/
+
+  self->currently_processing = false;
+  zix_sem_post (&self->processing_sem);
 
   return G_SOURCE_CONTINUE;
 }
@@ -1701,6 +1710,7 @@ recording_manager_new (void)
   mpmc_queue_reserve (
     self->event_queue, max_events);
 
+  zix_sem_init (&self->processing_sem, 1);
   self->source_id =
     g_timeout_add (
       12,

src/gui/backend/arranger_object.c
@@ -59,6 +59,7 @@
 #include "gui/widgets/velocity.h"
 #include "project.h"
 #include "utils/cairo.h"
+#include "utils/debug.h"
 #include "utils/dsp.h"
 #include "utils/error.h"
 #include "utils/flags.h"
@@ -218,7 +219,7 @@ arranger_object_get_num_loops (
   long loop_size =
     arranger_object_get_loop_length_in_frames (
       self);
-  g_return_val_if_fail (loop_size > 0, 0);
+  z_return_val_if_fail_cmp (loop_size, >, 0, 0);
   long full_size =
     arranger_object_get_length_in_frames (self);
   long loop_start =

commit 84cd4ac0d07fcca046e28cbff9d7c82cd424c1d3

fix event manager ptr array not being cleared on each run (regression from 1e45d1046)

src/audio/hardware_processor.c
@@ -265,12 +265,12 @@ hardware_processor_rescan_ext_ports (
   self->audio_ports =
     realloc (
       self->audio_ports,
-      self->ext_audio_ports_size *
+      MAX (1, self->ext_audio_ports_size) *
         sizeof (Port *));
   self->midi_ports =
     realloc (
       self->midi_ports,
-      self->ext_midi_ports_size *
+      MAX (1, self->ext_midi_ports_size) *
         sizeof (Port *));
   for (int i = 0; i < self->num_ext_audio_ports; i++)
     {

src/audio/track.c
@@ -58,6 +58,7 @@
 #include "gui/widgets/track.h"
 #include "project.h"
 #include "utils/arrays.h"
+#include "utils/debug.h"
 #include "utils/error.h"
 #include "utils/flags.h"
 #include "utils/io.h"
@@ -3828,10 +3829,11 @@ track_create_with_action (
   Track * track =
     tracklist_get_track (TRACKLIST, index);
   g_return_val_if_fail (
-    IS_TRACK_AND_NONNULL (track)
-    && track->type == type
-    && track->pos == index,
-    NULL);
+    IS_TRACK_AND_NONNULL (track), NULL);
+  z_return_val_if_fail_cmp (
+    track->type, ==, type, NULL);
+  z_return_val_if_fail_cmp (
+    track->pos, ==, index, NULL);
   return track;
 }
 

src/gui/backend/event_manager.c
@@ -1146,12 +1146,13 @@ on_plugin_visibility_changed (Plugin * pl)
 static inline void
 clean_duplicates_and_copy (
   EventManager * self,
-  GPtrArray *    events_arr,
-  int *          num_events)
+  GPtrArray *    events_arr)
 {
   MPMCQueue * q = self->mqueue;
   ZEvent * event;
-  *num_events = 0;
+
+  g_ptr_array_remove_range (
+    events_arr, 0, events_arr->len);
 
   /* only add events once to new array while
    * popping */
@@ -1160,7 +1161,7 @@ clean_duplicates_and_copy (
     {
       bool already_exists = false;
 
-      for (int i = 0; i < *num_events; i++)
+      for (guint i = 0; i < events_arr->len; i++)
         {
           ZEvent * cur_event =
             (ZEvent *)
@@ -1178,7 +1179,6 @@ clean_duplicates_and_copy (
       else
         {
           g_ptr_array_add (events_arr, event);
-          (*num_events) = *num_events + 1;
         }
     }
 }
@@ -1981,12 +1981,11 @@ process_events (void * data)
   EventManager * self = (EventManager *) data;
 
   ZEvent * ev;
-  int num_events = 0, i;
   clean_duplicates_and_copy (
-    self, self->events_arr, &num_events);
+    self, self->events_arr);
 
   /*g_message ("starting processing");*/
-  for (i = 0; i < num_events; i++)
+  for (guint i = 0; i < self->events_arr->len; i++)
     {
       if (i > 30)
         {
@@ -2017,7 +2016,7 @@ return_to_pool:
     }
   /*g_message ("processed %d events", i);*/
 
-  if (num_events > 6)
+  if (self->events_arr->len > 6)
     g_message ("More than 6 events processed. "
                "Optimization needed.");
 