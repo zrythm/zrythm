5986a10cd333af02df97b23e25d8ed397301300e
Alexandros Theodotou <alex@zrythm.org>
2022-11-01 23:50:10 +0900

    fix linear fade out audio function not working
    Fixes https://todo.sr.ht/~alextee/zrythm-bug/989.

--- src/audio/audio_function.c
+++ src/audio/audio_function.c
@@ -428,7 +428,7 @@ audio_function_apply (
     case AUDIO_FUNCTION_LINEAR_FADE_OUT:
       dsp_linear_fade_out_to (
         &frames[0], 0, num_frames * channels,
-        num_frames * channels, 1.f);
+        num_frames * channels, 0.f);
       break;
     case AUDIO_FUNCTION_NUDGE_LEFT:
       g_return_val_if_fail (num_frames > nudge_frames, -1);
