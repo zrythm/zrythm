faad74a45521d3fa126318c9ea8d10abe68ec79a
Alexandros Theodotou <alex@zrythm.org>
2022-11-06 16:17:00 +0900

    fix debug helper macros

--- inc/utils/debug.h
+++ inc/utils/debug.h
@@ -1,21 +1,5 @@
-/*
- * Copyright (C) 2021 Alexandros Theodotou <alex at zrythm dot org>
- *
- * This file is part of Zrythm
- *
- * Zrythm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Zrythm is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Zrythm.  If not, see <https://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: © 2021-2022 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-License-Identifier: LicenseRef-ZrythmLicense
 
 #ifndef __UTILS_DEBUG_H__
 #define __UTILS_DEBUG_H__
@@ -27,13 +11,13 @@
  */
 
 #define z_return_val_if_fail_cmp(a, comparator, b, val) \
-  if (!(G_LIKELY (a comparator b))) \
+  if (!(G_LIKELY ((a) comparator (b)))) \
     { \
       g_critical ( \
         "Assertion failed: %s (%" G_GINT64_FORMAT \
         ") " \
         "%s %s (%" G_GINT64_FORMAT ")", \
-        #a, (gint64) a, #comparator, #b, (gint64) b); \
+        #a, (gint64) (a), #comparator, #b, (gint64) (b)); \
       return val; \
     }
 
@@ -47,7 +31,7 @@
         "Assertion failed: %s (%" G_GINT64_FORMAT \
         ") " \
         "%s %s (%" G_GINT64_FORMAT ")", \
-        #a, (gint64) a, #comparator, #b, (gint64) b); \
+        #a, (gint64) (a), #comparator, #b, (gint64) (b)); \
     }
 
 /**
