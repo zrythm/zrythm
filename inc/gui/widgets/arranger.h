zrythm/zrythm

commit 6f78ae09866de7b3b73c985860c075a63befe08b

implement custom scrolling in arrangers
This handles scrolling manually by converting the arrangers into
canvases that "fake" where the current view is based on EditorSettings.

Squashed commit of the following:

commit 5414551bd6f0ed1bf8ff3861feb5d862d2cf3970
Author: Alexandros Theodotou <alex@zrythm.org>
Date:   Tue Nov 1 20:08:36 2022 +0900

    clean up

commit 57011fd0961a88c6765f63710ec3e602def90c09
Author: Alexandros Theodotou <alex@zrythm.org>
Date:   Tue Nov 1 13:42:21 2022 +0900

    fix arranger scrolling backwards when zooming in too quickly

commit 5a272b66760fa74ad5a701b6025f3bde79a1f27b
Author: Alexandros Theodotou <alex@zrythm.org>
Date:   Tue Nov 1 09:56:25 2022 +0900

    fix more things

commit 6bd81e3df31711ca99a30e1cf3fa75fa4fd461cf
Author: Alexandros Theodotou <alex@zrythm.org>
Date:   Tue Nov 1 09:35:31 2022 +0900

    fix midi arranger drawing

commit f6653e2cfa845bd93035f76272831acde1110421
Author: Alexandros Theodotou <alex@zrythm.org>
Date:   Tue Nov 1 09:30:08 2022 +0900

    take scroll offset into account while dragging

commit b1c8c6cae89b09993d0c5b1ffdc3eddff30377a3
Author: Alexandros Theodotou <alex@zrythm.org>
Date:   Tue Nov 1 01:19:31 2022 +0900

    fix lag when zooming on automation regions

commit 67a91ebae6e52a7de617073028a4f4cc5bb98e4f
Author: Alexandros Theodotou <alex@zrythm.org>
Date:   Mon Oct 31 22:59:01 2022 +0900

    increase max zoom level

commit a4309e8c3bc1f4b6058c315738439c3d485516f2
Author: Alexandros Theodotou <alex@zrythm.org>
Date:   Mon Oct 31 22:53:50 2022 +0900

    fix crash when zooming in too much on automation regions

commit 98701e38b6b6919fb099a71a8767ff44f63271b2
Author: Alexandros Theodotou <alex@zrythm.org>
Date:   Mon Oct 31 14:02:01 2022 +0900

    link midi scrolls

commit b6af7e7bc978814cdb2be84cdc643594821ee5c8
Author: Alexandros Theodotou <alex@zrythm.org>
Date:   Mon Oct 31 13:43:02 2022 +0900

    fix timeline drawing

commit c9ba9fdd4e68403b060b6eaff4b8d8a0152b81c8

inc/gui/widgets/arranger.h
@@ -124,6 +124,11 @@ typedef struct _ArrangerWidget
   double last_offset_x;
   double last_offset_y;
 
+  /** Whether there is an offset from a user scroll that should
+   * be added to the offset while dragging. */
+  double offset_x_from_scroll;
+  double offset_y_from_scroll;
+
   UiOverlayAction action;
 
   /** X-axis coordinate at start of drag. */
@@ -238,7 +243,7 @@ typedef struct _ArrangerWidget
   Position end_pos; ///< for moving regions
   gboolean key_is_pressed;
 
-  /** Current hovering positions. */
+  /** Current hovering positions (absolute). */
   double hover_x;
   double hover_y;
 
@@ -463,6 +468,12 @@ PURE const char *
 arranger_widget_get_type_str (ArrangerWidgetType type);
 
 /**
+ * Returns if the arranger can scroll vertically.
+ */
+bool
+arranger_widget_can_scroll_vertically (ArrangerWidget * self);
+
+/**
  * Creates a timeline widget using the given
  * timeline data.
  */
@@ -504,13 +515,6 @@ arranger_widget_get_cursor (ArrangerWidget * self);
  */
 void
 arranger_widget_refresh_cursor (ArrangerWidget * self);
-
-/**
- * Gets the corresponding scrolled window.
- */
-NONNULL
-GtkScrolledWindow *
-arranger_widget_get_scrolled_window (ArrangerWidget * self);
 
 /**
  * Get all objects currently present in the
@@ -662,7 +666,7 @@ arranger_widget_scroll_until_obj (
   int              horizontal,
   int              up,
   int              left,
-  double           padding);
+  int              padding);
 
 /**
  * Toggles the mute status of the selection, based
@@ -701,6 +705,14 @@ arranger_widget_set_highlight_rect (
  */
 EditorSettings *
 arranger_widget_get_editor_settings (ArrangerWidget * self);
+
+/**
+ * Get just the values, adjusted properly for special cases
+ * (like pinned timeline).
+ */
+EditorSettings
+arranger_widget_get_editor_setting_values (
+  ArrangerWidget * self);
 
 bool
 arranger_widget_is_playhead_visible (ArrangerWidget * self);

inc/gui/backend/editor_settings.h
@@ -1,21 +1,5 @@
-/*
- * Copyright (C) 2020-2021 Alexandros Theodotou <alex at zrythm dot org>
- *
- * This file is part of Zrythm
- *
- * Zrythm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Zrythm is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Zrythm.  If not, see <https://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: © 2020-2022 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-License-Identifier: LicenseRef-ZrythmLicense
 
 /**
  * @file
@@ -69,6 +53,28 @@ static const cyaml_schema_value_t editor_settings_schema = {
 
 void
 editor_settings_init (EditorSettings * self);
+
+void
+editor_settings_set_scroll_start_x (
+  EditorSettings * self,
+  int              x,
+  bool             validate);
+
+void
+editor_settings_set_scroll_start_y (
+  EditorSettings * self,
+  int              y,
+  bool             validate);
+
+/**
+ * Appends the given deltas to the scroll x/y values.
+ */
+void
+editor_settings_append_scroll (
+  EditorSettings * self,
+  int              dx,
+  int              dy,
+  bool             validate);
 
 /**
  * @}

inc/audio/region.h
@@ -687,6 +687,12 @@ ArrangerSelections *
 region_get_arranger_selections (ZRegion * self);
 
 /**
+ * Returns the arranger for editing the region's children.
+ */
+ArrangerWidget *
+region_get_arranger_for_children (ZRegion * self);
+
+/**
  * Sanity checking.
  *
  * @param frames_per_tick Frames per tick used when

inc/gui/backend/chord_editor.h
@@ -1,21 +1,5 @@
-/*
- * Copyright (C) 2019-2022 Alexandros Theodotou <alex at zrythm dot org>
- *
- * This file is part of Zrythm
- *
- * Zrythm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Zrythm is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Zrythm.  If not, see <https://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: © 2019-2022 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-License-Identifier: LicenseRef-ZrythmLicense
 
 /**
  * \file

inc/gui/widgets/audio_editor_space.h
@@ -1,21 +1,5 @@
-/*
- * Copyright (C) 2019 Alexandros Theodotou <alex at zrythm dot org>
- *
- * This file is part of Zrythm
- *
- * Zrythm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Zrythm is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: © 2019, 2022 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-License-Identifier: LicenseRef-ZrythmLicense
 
 /**
  * \file
@@ -56,10 +40,8 @@ typedef struct _AudioEditorSpaceWidget
 {
   GtkBox parent_instance;
 
-  GtkBox *            left_box;
-  GtkScrolledWindow * arranger_scroll;
-  GtkViewport *       arranger_viewport;
-  ArrangerWidget *    arranger;
+  GtkBox *         left_box;
+  ArrangerWidget * arranger;
 } AudioEditorSpaceWidget;
 
 void

inc/gui/widgets/automation_editor_space.h
@@ -1,21 +1,5 @@
-/*
- * Copyright (C) 2019-2020 Alexandros Theodotou <alex at zrythm dot org>
- *
- * This file is part of Zrythm
- *
- * Zrythm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Zrythm is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: © 2019-2020, 2022 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-License-Identifier: LicenseRef-ZrythmLicense
 
 /**
  * \file
@@ -67,9 +51,9 @@ typedef struct _AutomationEditorSpaceWidget
   GtkBox * left_box;
 
   /** The arranger. */
-  ArrangerWidget *    arranger;
-  GtkScrolledWindow * arranger_scroll;
-  GtkViewport *       arranger_viewport;
+  ArrangerWidget * arranger;
+  //GtkScrolledWindow * arranger_scroll;
+  //GtkViewport *       arranger_viewport;
 
   /* The legend on the left side. */
   AutomationEditorLegendWidget * legend;

inc/gui/widgets/automation_point.h
@@ -1,21 +1,5 @@
-/*
- * Copyright (C) 2018-2021 Alexandros Theodotou <alex at zrythm dot org>
- *
- * This file is part of Zrythm
- *
- * Zrythm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Zrythm is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Zrythm.  If not, see <https://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: © 2018-2021 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-License-Identifier: LicenseRef-ZrythmLicense
 
 /**
  * \file

commit 00d8ec8e01e69e8f0b3c48be487257cbc50f4148

fix sometimes arranger not taking off-screen objects into account
Regression from 6f78ae098.

inc/gui/widgets/arranger.h
@@ -296,10 +296,6 @@ typedef struct _ArrangerWidget
    */
   int resizing_range_start;
 
-  /** Cache for chord object height, used during
-   * child size allocation. */
-  /*int                  chord_obj_height; */
-
   AutomationTrack * hovered_at;
   TrackLane *       hovered_lane;
   Track *           hovered_track;
@@ -521,8 +517,7 @@ void
 arranger_widget_refresh_cursor (ArrangerWidget * self);
 
 /**
- * Get all objects currently present in the
- * arranger.
+ * Get all objects currently present in the arranger.
  */
 void
 arranger_widget_get_all_objects (
@@ -793,6 +788,12 @@ arranger_widget_finish_creating_item_from_action (
   ArrangerWidget * self,
   double           x,
   double           y);
+
+/**
+ * Returns the total height (including off-screen).
+ */
+int
+arranger_widget_get_total_height (ArrangerWidget * self);
 
 /**
  * @}

inc/schemas/gui/backend/editor_settings.h
@@ -1,21 +1,5 @@
-/*
- * Copyright (C) 2020-2021 Alexandros Theodotou <alex at zrythm dot org>
- *
- * This file is part of Zrythm
- *
- * Zrythm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Zrythm is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Zrythm.  If not, see <https://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: © 2020-2021 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-License-Identifier: LicenseRef-ZrythmLicense
 
 /**
  * @file

src/gui/widgets/arranger.c
@@ -4997,23 +4997,57 @@ arranger_widget_get_selections (ArrangerWidget * self)
 }
 
 /**
- * Get all objects currently present in the
- * arranger.
+ * Get all objects currently present in the arranger.
  */
 void
 arranger_widget_get_all_objects (
   ArrangerWidget * self,
   GPtrArray *      objs_arr)
 {
-  GdkRectangle rect = {
-    0,
-    0,
-    gtk_widget_get_allocated_width (GTK_WIDGET (self)),
-    gtk_widget_get_allocated_height (GTK_WIDGET (self)),
+  RulerWidget * ruler = arranger_widget_get_ruler (self);
+  GdkRectangle  rect = {
+     0,
+     0,
+     (int) ruler->total_px,
+     arranger_widget_get_total_height (self),
   };
 
   arranger_widget_get_hit_objects_in_rect (
     self, ARRANGER_OBJECT_TYPE_ALL, &rect, objs_arr);
+}
+
+/**
+ * Returns the total height (including off-screen).
+ */
+int
+arranger_widget_get_total_height (ArrangerWidget * self)
+{
+  int height = 0;
+  switch (self->type)
+    {
+    case TYPE (TIMELINE):
+      if (self->is_pinned)
+        {
+          height = gtk_widget_get_allocated_height (
+            GTK_WIDGET (self));
+        }
+      else
+        {
+          height = gtk_widget_get_allocated_height (
+            GTK_WIDGET (MW_TRACKLIST));
+        }
+      break;
+    case TYPE (MIDI):
+      height = gtk_widget_get_allocated_height (
+        GTK_WIDGET (MW_PIANO_ROLL_KEYS));
+      break;
+    default:
+      height =
+        gtk_widget_get_allocated_height (GTK_WIDGET (self));
+      break;
+    }
+
+  return height;
 }
 
 RulerWidget *