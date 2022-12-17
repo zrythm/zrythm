c81d42bc6cfd05cf706addde8389d79a0ee6d3df
Alexandros Theodotou <alex@zrythm.org>
2022-12-18 08:45:49 +0900

    fix ui test

--- tests/utils/ui.c
+++ tests/utils/ui.c
@@ -1,21 +1,5 @@
-/*
- * Copyright (C) 2020, 2022 Alexandros Theodotou <alex at zrythm dot org>
- *
- * This file is part of Zrythm
- *
- * Zrythm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Zrythm is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Zrythm.  If not, see <https://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: © 2020, 2022 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-License-Identifier: LicenseRef-ZrythmLicense
 
 #include "zrythm-test-config.h"
 
@@ -31,7 +15,7 @@ test_overlay_action_strings (void)
   /* verify that actions were not added/removed
    * without matching strings */
   g_assert_cmpstr (
-    ui_overlay_strings[NUM_UI_OVERLAY_ACTIONS], ==, "INVALID");
+    ui_overlay_strings[NUM_UI_OVERLAY_ACTIONS], ==, "--INVALID--");
 }
 
 int
