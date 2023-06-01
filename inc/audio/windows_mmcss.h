25c63572298dbda75f00e94772cc3cf2167056de
Alexandros Theodotou <alex@zrythm.org>
2023-06-02 06:59:16 +0900

    fix windows build

--- inc/audio/windows_mmcss.h
+++ inc/audio/windows_mmcss.h
@@ -1,21 +1,6 @@
+// SPDX-FileCopyrightText: © 2020, 2023 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-License-Identifier: LicenseRef-ZrythmLicense
 /*
- * Copyright (C) 2020 Alexandros Theodotou <alex at zrythm dot org>
- *
- * This file is part of Zrythm
- *
- * Zrythm is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Zrythm is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Zrythm.  If not, see <https://www.gnu.org/licenses/>.
- *
  * This file incorporates work covered by the following copyright and
  * permission notice:
  *
@@ -53,7 +38,7 @@ typedef enum AVRT_PRIORITY
   AVRT_PRIORITY_CRITICAL
 } AVRT_PRIORITY;
 
-enum error_codes
+typedef enum WindowsMMCSSErrorCode
 {
   ERROR_INVALID_TASK_NAME = 1550,
   ERROR_INVALID_TASK_INDEX = 1551
