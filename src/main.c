39fe9eccd5bf4bf4e7ca71fda6cda77307f89373
Alexandros Theodotou <alex@zrythm.org>
2024-02-09 20:46:06 +0900

    Revert "gsk: force old GL renderer on windows"
    This reverts commit c01ea35f1b8351e8fb8b1c04240ab7a3764a4d9e.
    
    The GTK releases that use ngl as the default renderer are not stable
    releases anyway so this change is not needed for now. Our binaries will
    be temporarily built from previous GTK versions too until the ngl
    renderer is stable.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,5 @@
-// clang-format off
-// SPDX-FileCopyrightText: © 2018-2022, 2024 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-FileCopyrightText: © 2018-2021 Alexandros Theodotou <alex@zrythm.org>
 // SPDX-License-Identifier: LicenseRef-ZrythmLicense
-// clang-format on
 
 #include "zrythm-config.h"
 
@@ -16,12 +14,6 @@
 int
 main (int argc, char ** argv)
 {
-#ifdef _WOE32
-  /* force old GL renderer on windows until this is fixed:
-   * https://gitlab.gnome.org/GNOME/gtk/-/issues/6401 */
-  g_setenv ("GSK_RENDERER", "gl", false);
-#endif
-
   LOG = log_new ();
 
   /* send activate signal */
