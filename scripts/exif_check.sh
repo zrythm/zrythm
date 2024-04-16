f9dd197bd773d816ee7f5a871b62e535287294b5
Alexandros Theodotou <alex@zrythm.org>
2024-04-16 17:59:32 +0900

    add missing license header

--- scripts/exif_check.sh
+++ scripts/exif_check.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: © 2024 Alexandros Theodotou <alex@zrythm.org>
+# SPDX-License-Identifier: CC0-1.0
 
 if exiftool -exif -if '$exif' doc/user/_static/img/*.png ; then
   echo 'Error: Exif found. Please remove exif data.'
