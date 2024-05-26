bacfa858c7a3dbc254f80dc503e3acae115b857f
Alexandros Theodotou <alex@zrythm.org>
2024-05-26 19:45:19 +0900

    fix compilation with gcc

--- src/pch/zrythm_pch.h
+++ src/pch/zrythm_pch.h
@@ -7,11 +7,16 @@
 #include <memory>
 
 #include <glib.h>
+#include <glib/gi18n.h>
 #include <gtk/gtk.h>
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshadow"
 #include <magic_enum_all.hpp>
+#pragma GCC diagnostic pop
 
 /* This also includes all native platform headers. */
-#include "ext/juce/juce.h"
+/* FIXME compilation fails with internal compiler errors if this is included */
+// #include "ext/juce/juce.h"
 
 #endif /* __PCH_ZRYTHM_PCH_H__ */
