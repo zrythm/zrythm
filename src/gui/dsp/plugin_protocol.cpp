8820bc3171296a8239a5607ab916dba2cdb4bb71
Alexandros Theodotou <alex@zrythm.org>
2025-04-20 12:18:55 +0900

    fix remaining missing licenses

--- src/gui/dsp/plugin_protocol.cpp
+++ src/gui/dsp/plugin_protocol.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: © 2024 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-License-Identifier: LicenseRef-ZrythmLicense
+
 #include "gui/dsp/plugin_protocol.h"
 #include "utils/logger.h"
 #include "utils/types.h"
