3a996c420a4cd8b90570b052d7317c574bcee2cb
Alexandros Theodotou <alex@zrythm.org>
2025-04-29 15:10:47 +0900

    add missing CTestCustom file

---
+++ CTestCustom.cmake
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: © 2025 Alexandros Theodotou <alex@zrythm.org>
+# SPDX-License-Identifier: LicenseRef-ZrythmLicense
+
+list(APPEND CTEST_CUSTOM_TESTS_IGNORE
+  # these are from soxr
+  20-bit-perfect-44100-192000
+  20-bit-perfect-192000-44100
+  20-bit-perfect-44100-65537
+  20-bit-perfect-65537-44100
+  28-bit-perfect-44100-192000
+  28-bit-perfect-192000-44100
+  28-bit-perfect-44100-65537
+  28-bit-perfect-65537-44100
+)
