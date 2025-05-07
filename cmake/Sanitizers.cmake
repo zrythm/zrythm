c2767af20f1342a41b280066c16a8a36fcf4339f
Alexandros Theodotou <alex@zrythm.org>
2025-05-07 10:52:21 +0900

    cmake: simplify sanitizers section

---
+++ cmake/Sanitizers.cmake
@@ -0,0 +1,99 @@
+# SPDX-FileCopyrightText: © 2025 Alexandros Theodotou <alex@zrythm.org>
+# SPDX-FileCopyrightText: lefticus (Jason Turner)
+# SPDX-License-Identifier: Unlicense
+# Taken and adapted from https://github.com/cpp-best-practices
+
+function(
+  enable_sanitizers
+  project_name
+  ENABLE_SANITIZER_ADDRESS
+  ENABLE_SANITIZER_LEAK
+  ENABLE_SANITIZER_UNDEFINED_BEHAVIOR
+  ENABLE_SANITIZER_THREAD
+  ENABLE_SANITIZER_MEMORY)
+
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+    set(SANITIZERS "")
+
+    if(${ENABLE_SANITIZER_ADDRESS})
+      list(APPEND SANITIZERS "address")
+      target_compile_options(${project_name} INTERFACE -fsanitize-address-use-after-scope)
+    endif()
+
+    if(${ENABLE_SANITIZER_LEAK})
+      list(APPEND SANITIZERS "leak")
+    endif()
+
+    if(${ENABLE_SANITIZER_UNDEFINED_BEHAVIOR})
+      list(APPEND SANITIZERS "undefined")
+    endif()
+
+    if(${ENABLE_SANITIZER_THREAD})
+      if("address" IN_LIST SANITIZERS OR "leak" IN_LIST SANITIZERS)
+        message(FATAL_ERROR "Thread sanitizer does not work with Address and Leak sanitizer enabled")
+      else()
+        list(APPEND SANITIZERS "thread")
+      endif()
+    endif()
+
+    if(${ENABLE_SANITIZER_MEMORY} AND CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+      message(
+        WARNING
+          "Memory sanitizer requires all the code (including libc++) to be MSan-instrumented otherwise it reports false positives"
+      )
+      if("address" IN_LIST SANITIZERS
+         OR "thread" IN_LIST SANITIZERS
+         OR "leak" IN_LIST SANITIZERS)
+        message(FATAL_ERROR "Memory sanitizer does not work with Address, Thread or Leak sanitizer enabled")
+      else()
+        list(APPEND SANITIZERS "memory")
+        target_compile_options(${project_name} INTERFACE -fsanitize-memory-track-origins)
+      endif()
+    endif()
+  elseif(MSVC)
+    if(${ENABLE_SANITIZER_ADDRESS})
+      list(APPEND SANITIZERS "address")
+    endif()
+    if(${ENABLE_SANITIZER_LEAK}
+       OR ${ENABLE_SANITIZER_UNDEFINED_BEHAVIOR}
+       OR ${ENABLE_SANITIZER_THREAD}
+       OR ${ENABLE_SANITIZER_MEMORY})
+      message(FATAL_ERROR "MSVC only supports address sanitizer")
+    endif()
+  endif()
+
+  list(
+    JOIN
+    SANITIZERS
+    ","
+    LIST_OF_SANITIZERS)
+
+  if(LIST_OF_SANITIZERS)
+    if(NOT
+       "${LIST_OF_SANITIZERS}"
+       STREQUAL
+       "")
+      if(NOT MSVC)
+        target_compile_options(${project_name} INTERFACE
+          -fsanitize=${LIST_OF_SANITIZERS}
+          -fno-omit-frame-pointer
+          -fno-optimize-sibling-calls
+        )
+        target_link_options(${project_name} INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
+      else()
+        string(FIND "$ENV{PATH}" "$ENV{VSINSTALLDIR}" index_of_vs_install_dir)
+        if("${index_of_vs_install_dir}" STREQUAL "-1")
+          message(
+            SEND_ERROR
+              "Using MSVC sanitizers requires setting the MSVC environment before building the project. Please manually open the MSVC command prompt and rebuild the project."
+          )
+        endif()
+        target_compile_options(${project_name} INTERFACE /fsanitize=${LIST_OF_SANITIZERS} /Zi /INCREMENTAL:NO)
+        target_compile_definitions(${project_name} INTERFACE _DISABLE_VECTOR_ANNOTATION _DISABLE_STRING_ANNOTATION)
+        target_link_options(${project_name} INTERFACE /INCREMENTAL:NO)
+      endif()
+    endif()
+  endif()
+
+endfunction()
+
