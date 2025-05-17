f113548a719b79ea98ab39fad88c400702bb1972
Alexandros Theodotou <alex@zrythm.org>
2025-05-18 08:57:39 +0900

    fix various mac app bundle issues

--- scripts/mac_codesign_and_notarize_app.sh
+++ scripts/mac_codesign_and_notarize_app.sh
@@ -20,8 +20,21 @@ ditto \
   -c -k --keepParent \
   "$app_bundle" \
   "$app_bundle_zip"
-xcrun notarytool submit \
-  "$app_bundle_zip" \
-  --keychain-profile "$notarization_keychain_profile" \
-  --wait
+
+if [ -n "$notarization_keychain_profile" ]; then
+  # Use keychain profile if provided
+  xcrun notarytool submit \
+    "$app_bundle_zip" \
+    --keychain-profile "$notarization_keychain_profile" \
+    --wait
+else
+  # Fall back to environment variables (used in CI)
+  xcrun notarytool submit \
+    "$app_bundle_zip" \
+    --apple-id "$APPLE_NOTARIZATION_APPLE_ID" \
+    --team-id "$APPLE_NOTARIZATION_TEAM_ID" \
+    --password "$APPLE_NOTARIZATION_ZRYTHM_APP_SPECIFIC_PASSWORD" \
+    --wait
+fi
+
 xcrun stapler staple "$app_bundle"
