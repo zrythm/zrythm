2949ec31a0370bed482084e6c64472f3cc88e609
Alexandros Theodotou <alex@zrythm.org>
2025-05-28 17:13:29 +0900

    include missing plugins.h PCH

---
+++ src/plugins/plugins.h
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: © 2025 Alexandros Theodotou <alex@zrythm.org>
+// SPDX-License-Identifier: LicenseRef-ZrythmLicense
+
+#pragma once
+
+#include "../dsp/dsp.h"
+#include "./out_of_process_scanner.h"
+#include "./plugin_configuration.h"
+#include "./plugin_descriptor.h"
+#include "./plugin_protocol.h"
+#include "./plugin_slot.h"
