8908e47b4c6c4d6bcb8e9b573ede4c59cdd96eed
Alexandros Theodotou <alex@zrythm.org>
2025-11-05 21:07:53 +0900

    remove unused fill_midi_events() API and fix shadowed declarations

--- src/structure/scenes/clip_slot.h
+++ src/structure/scenes/clip_slot.h
@@ -79,78 +79,7 @@ public:
   ClipSlotList (
     arrangement::ArrangerObjectRegistry &object_registry,
     const tracks::TrackCollection       &track_collection,
-    QObject *                            parent)
-      : QAbstractListModel (parent), object_registry_ (object_registry),
-        track_collection_ (track_collection)
-  {
-    // Initialize clip slots to match existing tracks
-    for (size_t i = 0; i < track_collection.track_count (); ++i)
-      {
-        clip_slots_.emplace_back (
-          utils::make_qobject_unique<ClipSlot> (object_registry_, this));
-      }
-
-    // Connect to track collection changes to keep clip slots synced
-    QObject::connect (
-      &track_collection, &tracks::TrackCollection::rowsInserted, this,
-      [this] (const QModelIndex &parent, int first, int last) {
-        // Insert clip slots for new tracks
-        beginInsertRows (parent, first, last);
-        for (int i = first; i <= last; ++i)
-          {
-            clip_slots_.insert (
-              clip_slots_.begin () + i,
-              utils::make_qobject_unique<ClipSlot> (object_registry_, this));
-          }
-        endInsertRows ();
-      });
-
-    QObject::connect (
-      &track_collection, &tracks::TrackCollection::rowsMoved, this,
-      [this] (
-        const QModelIndex &sourceParent, int sourceStart, int sourceEnd,
-        const QModelIndex &destinationParent, int destinationRow) {
-        // Move clip slots to match track movement
-        beginMoveRows (
-          sourceParent, sourceStart, sourceEnd, destinationParent,
-          destinationRow);
-
-        // Extract the range to move
-        std::vector<utils::QObjectUniquePtr<ClipSlot>> moved_slots;
-        for (int i = sourceStart; i <= sourceEnd; ++i)
-          {
-            moved_slots.emplace_back (std::move (clip_slots_[i]));
-          }
-
-        // Remove from original position
-        clip_slots_.erase (
-          clip_slots_.begin () + sourceStart,
-          clip_slots_.begin () + sourceEnd + 1);
-
-        // Insert at new position
-        int insert_pos = destinationRow;
-        if (destinationRow > sourceStart)
-          {
-            insert_pos = destinationRow - (sourceEnd - sourceStart + 1);
-          }
-        clip_slots_.insert (
-          clip_slots_.begin () + insert_pos,
-          std::make_move_iterator (moved_slots.begin ()),
-          std::make_move_iterator (moved_slots.end ()));
-
-        endMoveRows ();
-      });
-
-    QObject::connect (
-      &track_collection, &tracks::TrackCollection::rowsRemoved, this,
-      [this] (const QModelIndex &parent, int first, int last) {
-        // Remove clip slots for removed tracks
-        beginRemoveRows (parent, first, last);
-        clip_slots_.erase (
-          clip_slots_.begin () + first, clip_slots_.begin () + last + 1);
-        endRemoveRows ();
-      });
-  }
+    QObject *                            parent = nullptr);
   Z_DISABLE_COPY_MOVE (ClipSlotList)
 
   enum ClipSlotListRoles
