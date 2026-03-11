ef8e6416cb4708f6d13e3c1d2afb2ef91723b804
Alexandros Theodotou <alex@zrythm.org>
2026-03-11 12:37:21 +0900

    CTestCustom: ignore appstream test (commented out)

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2025 Alexandros Theodotou <alex@zrythm.org>
+# SPDX-FileCopyrightText: © 2025-2026 Alexandros Theodotou <alex@zrythm.org>
 # SPDX-License-Identifier: LicenseRef-ZrythmLicense
 
 list(APPEND CTEST_CUSTOM_TESTS_IGNORE
@@ -11,6 +11,8 @@ list(APPEND CTEST_CUSTOM_TESTS_IGNORE
   28-bit-perfect-192000-44100
   28-bit-perfect-44100-65537
   28-bit-perfect-65537-44100
+  # This depends on live websites
+  # appstream-cli-validate-appdata
 )
 
 # TODO: add tests to ignore from Memcheck here
