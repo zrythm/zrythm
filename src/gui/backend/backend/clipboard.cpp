4868580a3e4b98306dd326d512b10ecfb0dda890
Alexandros Theodotou <alex@zrythm.org>
2026-04-25 23:07:22 +0900

    drop some legacy code
    Signed-off-by: Alexandros Theodotou <alex@zrythm.org>

--- src/gui/backend/backend/clipboard.cpp
+++
@@ -1,4 +0,0 @@
-// SPDX-FileCopyrightText: © 2025 Alexandros Theodotou <alex@zrythm.org>
-// SPDX-License-Identifier: LicenseRef-ZrythmLicense
-
-#include "gui/backend/backend/clipboard.h"
--- tests/helpers/dir.h
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: © 2019 Alexandros Theodotou <alex@zrythm.org>
- *
- * SPDX-License-Identifier: LicenseRef-ZrythmLicense
- */
-
-/**
- * @dir helpers Test helpers.
- */
